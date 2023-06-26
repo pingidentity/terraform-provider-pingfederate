b685a29b9d8451bfc96f23ff7d319716ed046289
wesleymccollam <wesleymccollam@pingidentity.com>
2023-06-26 16:43:04 -0400

    session application session policy resource

---
+++ examples/pingfederate_session_applicationsessionpolicy/resource.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">=1.1"
+  required_providers {
+    pingfederate = {
+      version = "~> 0.0.1"
+      source = "pingidentity/pingfederate"
+    }
+  }
+}
+
+provider "pingfederate" {
+  username = "administrator"
+  password = "2FederateM0re"
+  https_host = "https://localhost:9999"
+}
+
+resource "pingfederate_session_applicationsessionpolicy" "sessionApplicationSessionPolicyExample" {
+	idle_timeout_mins = 60
+  max_timeout_mins = 60
+}
