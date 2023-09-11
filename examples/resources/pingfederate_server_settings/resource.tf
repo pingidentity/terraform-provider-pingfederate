1d3a52fe137018a258be6175e90fe46961aaa54d
PingDavidR <davidross@pingidentity.com>
2023-09-11 12:30:36 -0500

    docs & resource file

--- examples/resources/pingfederate_server_settings/resource.tf
+++ examples/resources/pingfederate_server_settings/resource.tf
@@ -3,68 +3,68 @@ terraform {
   required_providers {
     pingfederate = {
       version = "~> 0.0.4"
-      source = "pingidentity/pingfederate"
+      source  = "pingidentity/pingfederate"
     }
   }
 }
 
 provider "pingfederate" {
-  username = "administrator"
-  password = "2FederateM0re"
-  https_host = "https://localhost:9999"
+  username               = "administrator"
+  password               = "2FederateM0re"
+  https_host             = "https://localhost:9999"
   insecure_trust_all_tls = true
 }
 
 resource "pingfederate_server_settings" "serverSettingsExample" {
   contact_info = {
-    company= "example company"
-    email= "adminemail@company.com"
-    first_name= "Jane"
-    last_name= "Admin"
-    phone= "555-555-1222"
+    company    = "example company"
+    email      = "adminemail@company.com"
+    first_name = "Jane"
+    last_name  = "Admin"
+    phone      = "555-555-1222"
   }
 
-//  notifications= {
-//    license_events= {
-//      email_address= "license-events-email@company2.com"
-//      notification_publisher_ref= {
-//        id= "<uiInstanceID>"
-//      }
-//    }
-//    certificate_expirations= {
-//      email_address= "cert-expire-notifications@company2.com"
-//      initial_warning_period= 45
-//      final_warning_period= 7
-//      notification_publisher_ref= {
-//        id= "<uiInstanceID>"
-//      }
-//    }
-//    notify_admin_user_password_changes= true
-//    account_changes_notification_publisher_ref= {
-//      id= "<uiInstanceID>"
-//   }
-//   metadata_notification_settings= {
-//     email_address= "metadata-notification@company.com"
-//     notification_publisher_ref= {
-//       id= "<uiInstanceID>"
-//    }
-//   }
-// }
+  //  notifications= {
+  //    license_events= {
+  //      email_address= "license-events-email@company.com"
+  //      notification_publisher_ref= {
+  //        id= "<uiInstanceID>"
+  //      }
+  //    }
+  //    certificate_expirations= {
+  //      email_address= "cert-expire-notifications@company.com"
+  //      initial_warning_period= 45
+  //      final_warning_period= 7
+  //      notification_publisher_ref= {
+  //        id= "<uiInstanceID>"
+  //      }
+  //    }
+  //    notify_admin_user_password_changes= true
+  //    account_changes_notification_publisher_ref= {
+  //      id= "<uiInstanceID>"
+  //   }
+  //   metadata_notification_settings= {
+  //     email_address= "metadata-notification@company.com"
+  //     notification_publisher_ref= {
+  //       id= "<uiInstanceID>"
+  //    }
+  //   }
+  // }
 
   federation_info = {
-  // base_url must be standard URL format: http(s)://<company-or-hostname> with optional domain and port\
+    // base_url must be standard URL format: http(s)://<company-or-hostname> with optional domain and port\
     base_url = "https://localhost:9999"
-  // SAML entities have to be defined first
-    //saml2_entity_id= "urn:auth0:example:myserverconnection"
-    //saml1x_issuer_id= "pingidentity.com"
-    //saml1x_source_id should be a hex if supplied.  can be empty string or not set at all.
-    //saml1x_source_id= ""
-    //wsfed_realm= "myrealm"
+    // SAML entities have to be defined first
+    // saml2_entity_id  = "urn:auth0:example:myserverconnection"
+    // saml1x_issuer_id = "pingidentity.com"
+    // saml1x_source_id should be a hex if supplied.  Value can be empty string or not set at all.
+    // saml1x_source_id = ""
+    // wsfed_realm      = "myrealm"
   }
 
-  email_server= {
-    source_addr= "emailServerAdmin@company.com"
-    email_server= "myemailserver.company.com"
+  email_server = {
+    source_addr  = "emailServerAdmin@company.com"
+    email_server = "myemailserver.company.com"
     //use_ssl= true
     // cannot set both TLS and SSL at the same time.  SSL has priority
     //use_tls= true
