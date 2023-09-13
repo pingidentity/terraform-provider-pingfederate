57a180d972f89fba9f02333452ed35b139072d95
jamesburke <jamesburke@pingidentity.com>
2023-09-13 13:17:06 -0600

    wip

--- examples/resources/pingfederate_oauth_access_token_managers/resource.tf
+++ examples/resources/pingfederate_oauth_access_token_managers/resource.tf
@@ -1,3 +1,24 @@
-resource "pingfederate_oauth_access_token_managers" "oauthAccessTokenManagersExample" {
+terraform {
+  required_version = ">=1.1"
+  required_providers {
+    pingfederate = {
+      version = "~> 0.0.1"
+      source = "pingidentity/pingfederate"
+    }
+  }
+}
 
+provider "pingfederate" {
+  username = "administrator"
+  password = "2FederateM0re"
+  https_host = "https://localhost:9999"
+  insecure_trust_all_tls = true
+}
+
+resource "pingfederate_oauth_access_token_managers" "oauthAccessTokenManagersExample" {
+    id = "test_id"
+    name = "test_token_manager"
+    selection_settings = {
+        resource_uris = ["http://resource",]
+    }
 }
