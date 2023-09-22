a7b58980b0950a9602bed1b508cf249caea4c5ff
wesleymccollam <wesleymccollam@pingidentity.com>
2023-09-22 11:59:36 -0400

    add lists

--- internal/resource/config/common.go
+++ internal/resource/config/common.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/float64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -75,6 +76,16 @@ func SetAllAttributesToOptionalAndComputed(s *schema.Schema, exemptAttributes []
 				setAttr.Computed = true
 				setAttr.PlanModifiers = append(setAttr.PlanModifiers, setplanmodifier.UseStateForUnknown())
 				s.Attributes[key] = setAttr
+				continue
+			}
+			listAttr, ok := attribute.(schema.ListAttribute)
+			anyOk = ok || anyOk
+			if ok && (!listAttr.Computed || !listAttr.Optional) {
+				listAttr.Required = false
+				listAttr.Optional = true
+				listAttr.Computed = true
+				listAttr.PlanModifiers = append(listAttr.PlanModifiers, listplanmodifier.UseStateForUnknown())
+				s.Attributes[key] = listAttr
 				continue
 			}
 			boolAttr, ok := attribute.(schema.BoolAttribute)
