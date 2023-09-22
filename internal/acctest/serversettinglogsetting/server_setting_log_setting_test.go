71d26b701f8599625d5036acf2913727eb65a269
Jithin George <jithingeorge@pingidentity.com>
2023-09-22 12:21:40 -0700

    Resource Name,example and acctest singular fix

---
+++ internal/acctest/serversettinglogsetting/server_setting_log_setting_test.go
@@ -0,0 +1,139 @@
+package acctest_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/provider"
+)
+
+const serverSettingsLogSettingsId = "id"
+
+// Attributes to test with. Add optional properties to test here if desired.
+type serverSettingsLogSettingsResourceModel struct {
+	id                   string
+	logCategoriesEnabled bool
+}
+
+func TestAccServerSettingsLogSettings(t *testing.T) {
+	resourceName := "myServerSettingsLogSettings"
+	initialResourceModel := serverSettingsLogSettingsResourceModel{
+		id:                   serverSettingsLogSettingsId,
+		logCategoriesEnabled: false,
+	}
+	updatedResourceModel := serverSettingsLogSettingsResourceModel{
+		id:                   serverSettingsLogSettingsId,
+		logCategoriesEnabled: true,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"pingfederate": providerserver.NewProtocol6WithError(provider.New()),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServerSettingsLogSettings(resourceName, initialResourceModel),
+				Check:  testAccCheckExpectedServerSettingsLogSettingsAttributes(initialResourceModel),
+			},
+			{
+				// Test updating some fields
+				Config: testAccServerSettingsLogSettings(resourceName, updatedResourceModel),
+				Check:  testAccCheckExpectedServerSettingsLogSettingsAttributes(updatedResourceModel),
+			},
+			{
+				// Test importing the resource
+				Config:            testAccServerSettingsLogSettings(resourceName, updatedResourceModel),
+				ResourceName:      "pingfederate_server_setting_log_setting." + resourceName,
+				ImportStateId:     serverSettingsLogSettingsId,
+				ImportState:       true,
+				ImportStateVerify: false,
+			},
+		},
+	})
+}
+
+func testAccServerSettingsLogSettings(resourceName string, resourceModel serverSettingsLogSettingsResourceModel) string {
+	return fmt.Sprintf(`
+resource "pingfederate_server_setting_log_setting" "%[1]s" {
+  log_categories = [
+    {
+      id          = "core"
+      name        = "Core"
+      description = "Debug logging for core components."
+      enabled     = true
+    },
+    {
+      id          = "policytree"
+      name        = "Policy Tree"
+      description = "Policy tree debug logging."
+      enabled     = false
+    },
+    {
+      id          = "trustedcas"
+      name        = "Trusted CAs"
+      description = "Log PingFederate and JRE trusted CAs when they are loaded."
+      enabled     = true
+    },
+    {
+      id          = "xmlsig"
+      name        = "XML Signatures"
+      description = "Debug logging for XML signature operations."
+      enabled     = %[2]t
+    },
+    {
+      id          = "requestheaders"
+      name        = "HTTP Request Headers"
+      description = "Log HTTP request headers. Sensitive information, such as passwords, may be logged when this category is enabled."
+      enabled     = false
+    },
+    {
+      id          = "requestparams"
+      name        = "HTTP Request Parameters"
+      description = "Log HTTP GET request parameters. Sensitive information, such as passwords, may be logged when this category is enabled."
+      enabled     = true
+    },
+    {
+      id          = "restdatastore"
+      name        = "REST Data Store Requests and Responses"
+      description = "Log REST datastore requests and responses. Sensitive information, such as passwords, may be logged when this category is enabled."
+      enabled     = true
+    },
+  ]
+}`, resourceName,
+		resourceModel.logCategoriesEnabled,
+	)
+}
+
+// Test that the expected attributes are set on the PingFederate server
+func testAccCheckExpectedServerSettingsLogSettingsAttributes(config serverSettingsLogSettingsResourceModel) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceType := "ServerSettingsLogSettings"
+		testClient := acctest.TestClient()
+		ctx := acctest.TestBasicAuthContext()
+		response, _, err := testClient.ServerSettingsApi.GetLogSettings(ctx).Execute()
+
+		if err != nil {
+			return err
+		}
+		var logCategoryEnabledVal *bool
+		logCategories := response.GetLogCategories()
+		for i := 0; i < len(logCategories); i++ {
+			logCategoryId := logCategories[i].Id
+			if logCategoryId == "xmlsig" {
+				logCategoryEnabledVal = logCategories[i].Enabled
+			}
+		}
+		err = acctest.TestAttributesMatchBool(resourceType, &config.id, "enabled", config.logCategoriesEnabled, *logCategoryEnabledVal)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
---
+++ internal/acctest/serversettingsystemkey/server_setting_system_key_test.go
@@ -0,0 +1,106 @@
+package acctest_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/provider"
+)
+
+const serverSettingsSystemKeysId = "id"
+
+// Attributes to test with. Add optional properties to test here if desired.
+type serverSettingsSystemKeysResourceModel struct {
+	id                      string
+	currentEncryptedKeyData string
+	pendingEncryptedKeyData string
+}
+
+func TestAccServerSettingsSystemKeys(t *testing.T) {
+	resourceName := "myServerSettingsSystemKeys"
+	initialResourceModel := serverSettingsSystemKeysResourceModel{
+		id:                      serverSettingsSystemKeysId,
+		currentEncryptedKeyData: "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2Iiwia2lkIjoiUWVzOVR5eTV5WiIsInZlcnNpb24iOiIxMS4yLjUuMCIsInppcCI6IkRFRiJ9..4Q-LeikGMQ-5dVVRMMDyfw.JLR4Yg1FfmaTdOpVHZ1V1BypiguCuKawnJsUD33weL3nYRvyEPFgMCuBV72GC-HG.2b2T22iR040xI4ro-Iemeg",
+		pendingEncryptedKeyData: "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2Iiwia2lkIjoiUWVzOVR5eTV5WiIsInZlcnNpb24iOiIxMS4yLjUuMCIsInppcCI6IkRFRiJ9..J1yaOm2OdYCUDN402iIKPQ.LlpjecXwfHDiFJl_K6O57Mzp1RZxHN-TAbpKnypkRfeL1XgTHZrUkPgxO3ZcU7fb.q-X1zzd-de5svqDRbAE0lw",
+	}
+	updatedResourceModel := serverSettingsSystemKeysResourceModel{
+		id:                      serverSettingsSystemKeysId,
+		currentEncryptedKeyData: "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2Iiwia2lkIjoiUWVzOVR5eTV5WiIsInZlcnNpb24iOiIxMS4yLjUuMCIsInppcCI6IkRFRiJ9..J1yaOm2OdYCUDN402iIKPQ.LlpjecXwfHDiFJl_K6O57Mzp1RZxHN-TAbpKnypkRfeL1XgTHZrUkPgxO3ZcU7fb.q-X1zzd-de5svqDRbAE0lw",
+		pendingEncryptedKeyData: "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2Iiwia2lkIjoiUWVzOVR5eTV5WiIsInZlcnNpb24iOiIxMS4yLjUuMCIsInppcCI6IkRFRiJ9..4Q-LeikGMQ-5dVVRMMDyfw.JLR4Yg1FfmaTdOpVHZ1V1BypiguCuKawnJsUD33weL3nYRvyEPFgMCuBV72GC-HG.2b2T22iR040xI4ro-Iemeg",
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"pingfederate": providerserver.NewProtocol6WithError(provider.New()),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServerSettingsSystemKeys(resourceName, initialResourceModel),
+				Check:  testAccCheckExpectedServerSettingsSystemKeysAttributes(initialResourceModel),
+			},
+			{
+				// Test updating some fields
+				Config: testAccServerSettingsSystemKeys(resourceName, updatedResourceModel),
+				Check:  testAccCheckExpectedServerSettingsSystemKeysAttributes(updatedResourceModel),
+			},
+			{
+				// Test importing the resource
+				Config:            testAccServerSettingsSystemKeys(resourceName, updatedResourceModel),
+				ResourceName:      "pingfederate_server_setting_system_key." + resourceName,
+				ImportStateId:     serverSettingsSystemKeysId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccServerSettingsSystemKeys(resourceName string, resourceModel serverSettingsSystemKeysResourceModel) string {
+	return fmt.Sprintf(`
+resource "pingfederate_server_setting_system_key" "%[1]s" {
+  current = {
+    encrypted_key_data = "%[2]s"
+  }
+  pending = {
+    encrypted_key_data = "%[3]s"
+  }
+
+}`, resourceName,
+		resourceModel.currentEncryptedKeyData,
+		resourceModel.pendingEncryptedKeyData,
+	)
+}
+
+// Test that the expected attributes are set on the PingFederate server
+func testAccCheckExpectedServerSettingsSystemKeysAttributes(config serverSettingsSystemKeysResourceModel) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceType := "ServerSettingsSystemKeys"
+		testClient := acctest.TestClient()
+		ctx := acctest.TestBasicAuthContext()
+		response, _, err := testClient.ServerSettingsApi.GetSystemKeys(ctx).Execute()
+
+		if err != nil {
+			return err
+		}
+
+		// Verify that attributes have expected values
+		currentEncryptedKeyData := response.Current.EncryptedKeyData
+		err = acctest.TestAttributesMatchString(resourceType, &config.id, "encrypted_key_data", config.currentEncryptedKeyData, *currentEncryptedKeyData)
+		if err != nil {
+			return err
+		}
+		pendingEncryptedKeyData := response.Pending.EncryptedKeyData
+		err = acctest.TestAttributesMatchString(resourceType, &config.id, "encrypted_key_data", config.pendingEncryptedKeyData, *pendingEncryptedKeyData)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
