9c1a5921973576557d3a8a0fbb21a22781864acf
jamesburke <jamesburke@pingidentity.com>
2023-09-25 11:37:52 -0700

    wip

--- examples/resources/pingfederate_server_settings_system_keys/resource.tf
+++ examples/resources/pingfederate_server_settings_system_keys/resource.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">=1.1"
-  required_providers {
-    pingfederate = {
-      version = "~> 0.0.1"
-      source  = "pingidentity/pingfederate"
-    }
-  }
-}
-provider "pingfederate" {
-  username               = "administrator"
-  password               = "2FederateM0re"
-  https_host             = "https://localhost:9999"
-  insecure_trust_all_tls = true
-}
-
 # WARNING! You will need to secure your state file properly when using this resource! #
 # Please refer to the link below on how to best store state files and data within. #
 # https://developer.hashicorp.com/terraform/plugin/best-practices/sensitive-state #
