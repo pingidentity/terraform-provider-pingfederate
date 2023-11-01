541a9f74859774d95bca9fb24cc41b66b9ebfad5
Henry Recker <henryrecker@pingidentity.com>
2023-11-01 13:43:31 -0500

    Add missing example for session application session policy

--- examples/resources/pingfederate_session_application_session_policies/resource.tf
+++ examples/resources/pingfederate_session_application_session_policies/resource.tf
@@ -1,4 +1,4 @@
-resource "pingfederate_session_authentication_session_policies_global" "sessionApplicationSessionPolicyExample" {
+resource "pingfederate_session_authentication_session_policies_global" "sessionAuthenticationSessionPoliciesGlobalExample" {
   idle_timeout_mins = 60
   max_timeout_mins  = 60
 }
---
+++ examples/resources/pingfederate_session_application_session_policy/resource.tf
@@ -0,0 +1,4 @@
+resource "pingfederate_session_application_session_policy" "sessionApplicationSessionPolicyExample" {
+  idle_timeout_mins = 60
+  max_timeout_mins  = 60
+}
