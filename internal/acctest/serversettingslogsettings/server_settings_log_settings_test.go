e153b0a730636c395cbfa823e634242857912452
Henry Recker <henryrecker@pingidentity.com>
2023-11-03 11:39:05 -0500

    Add defaults where possible for server settings log settings resource

--- internal/acctest/serversettingslogsettings/server_settings_log_settings_test.go
+++ internal/acctest/serversettingslogsettings/server_settings_log_settings_test.go
@@ -58,44 +58,30 @@ resource "pingfederate_server_settings_log_settings" "%[1]s" {
   log_categories = [
     {
       id          = "policytree"
-      name        = "Policy Tree"
-      description = "Policy tree debug logging."
       enabled     = false
     },
     {
       id          = "core"
-      name        = "Core"
-      description = "Debug logging for core components."
       enabled     = true
     },
     {
       id          = "trustedcas"
-      name        = "Trusted CAs"
-      description = "Log PingFederate and JRE trusted CAs when they are loaded."
       enabled     = true
     },
     {
       id          = "xmlsig"
-      name        = "XML Signatures"
-      description = "Debug logging for XML signature operations."
       enabled     = %[2]t
     },
     {
       id          = "requestheaders"
-      name        = "HTTP Request Headers"
-      description = "Log HTTP request headers. Sensitive information, such as passwords, may be logged when this category is enabled."
       enabled     = false
     },
     {
       id          = "requestparams"
-      name        = "HTTP Request Parameters"
-      description = "Log HTTP GET request parameters. Sensitive information, such as passwords, may be logged when this category is enabled."
       enabled     = true
     },
     {
       id          = "restdatastore"
-      name        = "REST Data Store Requests and Responses"
-      description = "Log REST datastore requests and responses. Sensitive information, such as passwords, may be logged when this category is enabled."
       enabled     = true
     },
   ]
