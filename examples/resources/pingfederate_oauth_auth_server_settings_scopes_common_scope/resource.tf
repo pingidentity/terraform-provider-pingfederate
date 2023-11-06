e37765ffdbb1d9eba4ca22fa5ad0f6f5c73315da
PingJamesB <107875250+PingJamesB@users.noreply.github.com>
2023-11-06 10:44:46 -0700

    add oauthAuthServerSettingsScopesCommonScope data source (#85)
    add oauthauthserversettingsscopescommonscope data source

--- examples/resources/pingfederate_oauth_auth_server_settings_scopes_common_scope/resource.tf
+++ examples/resources/pingfederate_oauth_auth_server_settings_scopes_common_scope/resource.tf
@@ -1,4 +1,4 @@
-resource "pingfederate_oauth_auth_server_settings_scopes_common_scope" "oauthAuthServerSettingsScopesCommonScopeExample" {
+resource "pingfederate_oauth_auth_server_settings_scopes_common_scope" "myOauthAuthServerSettingsScopesCommonScope" {
   dynamic     = true
   description = "example"
   name        = "*exampleCommonScope"
