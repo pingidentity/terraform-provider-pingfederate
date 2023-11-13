bc1f9b58405df578d27696e73dc548e79fa81910
Henry Recker <henryrecker@pingidentity.com>
2023-11-13 15:57:23 -0600

    Test improvements for local identity identity profile resource

--- internal/acctest/localidentityidentityprofile/local_identity_identity_profile_test.go
+++ internal/acctest/localidentityidentityprofile/local_identity_identity_profile_test.go
@@ -2,24 +2,356 @@ package acctest_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	client "github.com/pingidentity/pingfederate-go-client/v1125/configurationapi"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest/common/pointers"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/provider"
 )
 
-const localIdentityIdentityProfilesId = "test"
+const localIdentityIdentityProfilesId = "testLocalIdProfile"
 
 // Attributes to test with. Add optional properties to test here if desired.
 type localIdentityIdentityProfilesResourceModel struct {
-	id                  string
-	name                string
-	registrationEnabled bool
-	profileEnabled      bool
+	id                      string
+	name                    string
+	registrationEnabled     bool
+	profileEnabled          bool
+	authSources             []string
+	authSourceUpdatePolicy  *client.LocalIdentityAuthSourceUpdatePolicy
+	registrationConfig      *client.RegistrationConfig
+	profileConfig           *client.ProfileConfig
+	fieldConfig             *client.FieldConfig
+	emailVerificationConfig *client.EmailVerificationConfig
+	dataStoreConfig         *client.LdapDataStoreConfig
+}
+
+func authSourcesHcl(sources []string) string {
+	if len(sources) == 0 {
+		// Just leave out empty auth sources for the sake of this test
+		return ""
+	}
+
+	var hcl strings.Builder
+	hcl.WriteString("auth_sources = [\n")
+	for _, source := range sources {
+		hcl.WriteString("    {\n")
+		hcl.WriteString(fmt.Sprintf("        source = \"%s\"\n", source))
+		hcl.WriteString("    },\n")
+	}
+	hcl.WriteString("]\n")
+
+	return hcl.String()
+}
+
+func updatedAuthSourceUpdatePolicy() *client.LocalIdentityAuthSourceUpdatePolicy {
+	return &client.LocalIdentityAuthSourceUpdatePolicy{
+		StoreAttributes:  pointers.Bool(false),
+		RetainAttributes: pointers.Bool(false),
+		UpdateAttributes: pointers.Bool(false),
+		UpdateInterval:   pointers.Float64(0),
+	}
+}
+
+func authSourceUpdatePolicyHcl(policy *client.LocalIdentityAuthSourceUpdatePolicy) string {
+	if policy == nil {
+		return ""
+	}
+
+	return fmt.Sprintf(`
+	auth_source_update_policy = {
+		store_attributes = %[1]t
+		retain_attributes = %[2]t
+		update_attributes = %[3]t
+		update_interval = %[4]f
+	}
+	`, *policy.StoreAttributes, *policy.RetainAttributes, *policy.UpdateAttributes, *policy.UpdateInterval)
+}
+
+func updatedRegistrationConfig() *client.RegistrationConfig {
+	return &client.RegistrationConfig{
+		CaptchaEnabled: pointers.Bool(true),
+		CaptchaProviderRef: &client.ResourceLink{
+			Id: "exampleCaptchaProvider",
+		},
+		TemplateName:                        "local.identity.registration.html",
+		CreateAuthnSessionAfterRegistration: pointers.Bool(true),
+		UsernameField:                       pointers.String("cn"),
+		ThisIsMyDeviceEnabled:               pointers.Bool(false),
+	}
+}
+
+func registrationConfigHcl(config *client.RegistrationConfig) string {
+	if config == nil {
+		return ""
+	}
+
+	return fmt.Sprintf(`
+	registration_config = {
+		captcha_enabled = %[1]t
+		captcha_provider_ref = {
+			id = "%[2]s"
+		}
+		template_name = "%[3]s"
+		create_authn_session_after_registration = %[4]t
+		username_field = "%[5]s"
+		this_is_my_device_enabled = %[6]t
+	}
+	`, *config.CaptchaEnabled,
+		config.CaptchaProviderRef.Id,
+		config.TemplateName,
+		*config.CreateAuthnSessionAfterRegistration,
+		*config.UsernameField,
+		*config.ThisIsMyDeviceEnabled)
+}
+
+func updatedProfileConfig() *client.ProfileConfig {
+	return &client.ProfileConfig{
+		DeleteIdentityEnabled: pointers.Bool(true),
+		TemplateName:          "local.identity.profile.html",
+	}
+}
+
+func profileConfigHcl(config *client.ProfileConfig) string {
+	if config == nil {
+		return ""
+	}
+
+	return fmt.Sprintf(`
+	profile_config = {
+		delete_identity_enabled = %[1]t
+		template_name = "%[2]s"
+	}
+	`, *config.DeleteIdentityEnabled, config.TemplateName)
+}
+
+func updatedFieldConfig() *client.FieldConfig {
+	emailAttrs := map[string]bool{
+		"Read-Only":       false,
+		"Required":        true,
+		"Unique ID Field": true,
+		"Mask Log Values": false,
+	}
+	textAttrs := map[string]bool{
+		"Read-Only":       false,
+		"Required":        true,
+		"Unique ID Field": false,
+		"Mask Log Values": false,
+	}
+	hiddenAttrs := map[string]bool{
+		"Unique ID Field": false,
+		"Mask Log Values": false,
+	}
+
+	return &client.FieldConfig{
+		Fields: []client.LocalIdentityField{
+			{
+				Type:                  "EMAIL",
+				Id:                    "mail",
+				Label:                 "Email address",
+				RegistrationPageField: pointers.Bool(true),
+				ProfilePageField:      pointers.Bool(true),
+				Attributes:            &emailAttrs,
+			},
+			{
+				Type:                  "TEXT",
+				Id:                    "cn",
+				Label:                 "First Name",
+				RegistrationPageField: pointers.Bool(true),
+				ProfilePageField:      pointers.Bool(true),
+				Attributes:            &textAttrs,
+			},
+			{
+				Type:                  "HIDDEN",
+				Id:                    "entryUUID",
+				Label:                 "entryUUID",
+				RegistrationPageField: pointers.Bool(true),
+				ProfilePageField:      pointers.Bool(true),
+				Attributes:            &hiddenAttrs,
+			},
+		},
+		StripSpaceFromUniqueField: pointers.Bool(true),
+	}
+}
+
+func fieldHcl(field client.LocalIdentityField) string {
+	var attributes strings.Builder
+	if field.Attributes != nil {
+		attributes.WriteString("attributes = {\n")
+		for name, val := range *field.Attributes {
+			attributes.WriteString(fmt.Sprintf("\"%s\" = %t,\n", name, val))
+		}
+		attributes.WriteString("}\n")
+	}
+
+	return fmt.Sprintf(`
+	{
+		type = "%s"
+		id = "%s"
+		label = "%s"
+		registration_page_field = %t
+		profile_page_field = %t
+		%s
+	},
+	`, field.Type,
+		field.Id,
+		field.Label,
+		*field.RegistrationPageField,
+		*field.ProfilePageField,
+		attributes.String())
+}
+
+func fieldConfigHcl(config *client.FieldConfig) string {
+	if config == nil {
+		return ""
+	}
+
+	var fields strings.Builder
+	fields.WriteString("fields = [\n")
+	for _, field := range config.Fields {
+		fields.WriteString(fieldHcl(field))
+	}
+	fields.WriteString("]\n")
+
+	return fmt.Sprintf(`
+	field_config = {
+		%s
+		strip_space_from_unique_field = %t
+	}
+	`, fields.String(), *config.StripSpaceFromUniqueField)
+}
+
+func updatedEmailVerificationConfig() *client.EmailVerificationConfig {
+	return &client.EmailVerificationConfig{
+		EmailVerificationEnabled:             pointers.Bool(true),
+		VerifyEmailTemplateName:              pointers.String("message-template-email-ownership-verification.html"),
+		EmailVerificationSuccessTemplateName: pointers.String("local.identity.email.verification.success.html"),
+		EmailVerificationErrorTemplateName:   pointers.String("local.identity.email.verification.error.html"),
+		EmailVerificationType:                pointers.String("OTP"),
+		AllowedOtpCharacterSet:               pointers.String("23456789BCDFGHJKMNPQRSTVWXZbcdfghjkmnpqrstvwxz"),
+		EmailVerificationOtpTemplateName:     pointers.String("message-template-email-ownership-verification.html"),
+		OtpLength:                            pointers.Int64(8),
+		OtpRetryAttempts:                     pointers.Int64(3),
+		OtpTimeToLive:                        pointers.Int64(1440),
+		FieldForEmailToVerify:                "mail",
+		FieldStoringVerificationStatus:       "entryUUID",
+		NotificationPublisherRef: &client.ResourceLink{
+			Id: "exampleSmtpPublisher",
+		},
+		RequireVerifiedEmail: pointers.Bool(true),
+	}
+}
+
+func emailVerificationConfigHcl(config *client.EmailVerificationConfig) string {
+	if config == nil {
+		return ""
+	}
+
+	return fmt.Sprintf(`
+	email_verification_config = {
+		email_verification_enabled = %t
+		verify_email_template_name = "%s"
+		email_verification_success_template_name = "%s"
+		email_verification_error_template_name   = "%s"
+		email_verification_type              = "%s"
+		allowed_otp_character_set            = "%s"
+		email_verification_otp_template_name = "%s"
+		otp_length                           = %d
+		otp_retry_attempts                   = %d
+		otp_time_to_live                     = %d
+		field_for_email_to_verify         = "%s"
+		field_storing_verification_status = "%s"
+		notification_publisher_ref = {
+		  id = "%s",
+		}
+		require_verified_email = %t
+	}`, *config.EmailVerificationEnabled,
+		*config.VerifyEmailTemplateName,
+		*config.EmailVerificationSuccessTemplateName,
+		*config.EmailVerificationErrorTemplateName,
+		*config.EmailVerificationType,
+		*config.AllowedOtpCharacterSet,
+		*config.EmailVerificationOtpTemplateName,
+		*config.OtpLength,
+		*config.OtpRetryAttempts,
+		*config.OtpTimeToLive,
+		config.FieldForEmailToVerify,
+		config.FieldStoringVerificationStatus,
+		config.NotificationPublisherRef.Id,
+		*config.RequireVerifiedEmail)
+}
+
+func updatedDataStoreConfig() *client.LdapDataStoreConfig {
+	return &client.LdapDataStoreConfig{
+		Type: "LDAP",
+		DataStoreRef: client.ResourceLink{
+			Id: "pingdirectory",
+		},
+		BaseDn:        "ou=people,dc=example,dc=com",
+		CreatePattern: "uid=$${mail}",
+		ObjectClass:   "inetOrgPerson",
+		DataStoreMapping: map[string]client.DataStoreAttribute{
+			"entryUUID": {
+				Type: "LDAP",
+				Name: "entryUUID",
+			},
+			"cn": {
+				Type: "LDAP",
+				Name: "cn",
+			},
+			"mail": {
+				Type: "LDAP",
+				Name: "mail",
+			},
+		},
+	}
+}
+
+func dataStoreAttributeHcl(attribute client.DataStoreAttribute) string {
+	return fmt.Sprintf(`
+	"%[1]s" = {
+		type = "%[2]s"
+		name = "%[1]s"
+		metadata = {}
+	}
+	`, attribute.Name, attribute.Type)
+}
+
+func dataStoreConfigHcl(config *client.LdapDataStoreConfig) string {
+	if config == nil {
+		return ""
+	}
+
+	var dataStoreMapping strings.Builder
+	dataStoreMapping.WriteString("data_store_mapping = {\n")
+	for _, attr := range config.DataStoreMapping {
+		dataStoreMapping.WriteString(dataStoreAttributeHcl(attr))
+	}
+	dataStoreMapping.WriteString("}\n")
+
+	return fmt.Sprintf(`
+	data_store_config = {
+		type = "%s"
+		data_store_ref = {
+			id = "%s"
+		}
+		base_dn = "%s"
+		create_pattern = "%s"
+		object_class = "%s"
+		%s
+	}
+	`, config.Type,
+		config.DataStoreRef.Id,
+		config.BaseDn,
+		config.CreatePattern,
+		config.ObjectClass,
+		dataStoreMapping.String())
 }
 
 func TestAccLocalIdentityIdentityProfiles(t *testing.T) {
@@ -32,10 +364,17 @@ func TestAccLocalIdentityIdentityProfiles(t *testing.T) {
 		profileEnabled:      false,
 	}
 	updatedResourceModel := localIdentityIdentityProfilesResourceModel{
-		id:                  localIdentityIdentityProfilesId,
-		name:                "example1",
-		registrationEnabled: false,
-		profileEnabled:      false,
+		id:                      localIdentityIdentityProfilesId,
+		name:                    "example1",
+		registrationEnabled:     true,
+		profileEnabled:          true,
+		authSources:             []string{"example", "test"},
+		authSourceUpdatePolicy:  updatedAuthSourceUpdatePolicy(),
+		registrationConfig:      updatedRegistrationConfig(),
+		profileConfig:           updatedProfileConfig(),
+		fieldConfig:             updatedFieldConfig(),
+		emailVerificationConfig: updatedEmailVerificationConfig(),
+		dataStoreConfig:         updatedDataStoreConfig(),
 	}
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
@@ -61,6 +400,11 @@ func TestAccLocalIdentityIdentityProfiles(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
+			{
+				// Back to minimal model
+				Config: testAccLocalIdentityIdentityProfiles(resourceName, initialResourceModel),
+				Check:  testAccCheckExpectedLocalIdentityIdentityProfilesAttributes(initialResourceModel),
+			},
 		},
 	})
 }
@@ -71,7 +415,7 @@ resource "pingfederate_authentication_policy_contract" "authenticationPolicyCont
   contract_id         = "%[2]s"
   core_attributes     = [{ name = "subject" }]
   extended_attributes = [{ name = "extended_attribute" }, { name = "extended_attribute2" }]
-  name                = "example"
+  name                = "%[2]s"
 }
 
 resource "pingfederate_local_identity_identity_profile" "%[1]s" {
@@ -82,7 +426,13 @@ resource "pingfederate_local_identity_identity_profile" "%[1]s" {
   }
   registration_enabled = %[4]t
   profile_enabled      = %[5]t
-
+  %[6]s
+  %[7]s
+  %[8]s
+  %[9]s
+  %[10]s
+  %[11]s
+  %[12]s
 }
 
 data "pingfederate_local_identity_identity_profile" "%[1]s" {
@@ -92,6 +442,13 @@ data "pingfederate_local_identity_identity_profile" "%[1]s" {
 		resourceModel.name,
 		resourceModel.registrationEnabled,
 		resourceModel.profileEnabled,
+		authSourcesHcl(resourceModel.authSources),
+		authSourceUpdatePolicyHcl(resourceModel.authSourceUpdatePolicy),
+		registrationConfigHcl(resourceModel.registrationConfig),
+		profileConfigHcl(resourceModel.profileConfig),
+		fieldConfigHcl(resourceModel.fieldConfig),
+		emailVerificationConfigHcl(resourceModel.emailVerificationConfig),
+		dataStoreConfigHcl(resourceModel.dataStoreConfig),
 	)
 }
 
@@ -129,6 +486,22 @@ func testAccCheckExpectedLocalIdentityIdentityProfilesAttributes(config localIde
 			config.profileEnabled, *response.ProfileEnabled)
 		if err != nil {
 			return err
+		}
+
+		if config.registrationConfig != nil {
+			err = acctest.TestAttributesMatchBool(resourceType, &config.id, "registration_config.captcha_enabled",
+				*config.registrationConfig.CaptchaEnabled, *response.RegistrationConfig.CaptchaEnabled)
+			if err != nil {
+				return err
+			}
+		}
+
+		if config.dataStoreConfig != nil {
+			err = acctest.TestAttributesMatchString(resourceType, &config.id, "data_store_config.base_dn",
+				config.dataStoreConfig.BaseDn, *&response.DataStoreConfig.BaseDn)
+			if err != nil {
+				return err
+			}
 		}
 		return nil
 	}
