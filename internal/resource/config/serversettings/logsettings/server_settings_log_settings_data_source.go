795b0a24566579a595065baebe4a490ff9a4ea0e
PingDavidR <davidross@pingidentity.com>
2023-11-15 12:44:18 -0600

    wip

--- internal/resource/config/serversettings/logsettings/server_settings_log_settings_data_source.go
+++ internal/resource/config/serversettings/logsettings/server_settings_log_settings_data_source.go
@@ -5,115 +5,65 @@ import (
 	"encoding/json"
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setdefault"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	client "github.com/pingidentity/pingfederate-go-client/v1125/configurationapi"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/datasource/common/id"
 	internaljson "github.com/pingidentity/terraform-provider-pingfederate/internal/json"
-	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/common/id"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config"
 	internaltypes "github.com/pingidentity/terraform-provider-pingfederate/internal/types"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
-	_ resource.Resource                = &serverSettingsLogSettingsResource{}
-	_ resource.ResourceWithConfigure   = &serverSettingsLogSettingsResource{}
-	_ resource.ResourceWithImportState = &serverSettingsLogSettingsResource{}
-
-	logCategoriesAttrTypes = map[string]attr.Type{
-		"id":          basetypes.StringType{},
-		"name":        basetypes.StringType{},
-		"description": basetypes.StringType{},
-		"enabled":     basetypes.BoolType{},
-	}
-
-	logCategoriesDefault, _ = types.SetValue(types.ObjectType{AttrTypes: logCategoriesAttrTypes}, []attr.Value{
-		createDefaultLogCategoryObject("xmlsig"),
-		createDefaultLogCategoryObject("core"),
-		createDefaultLogCategoryObject("requestparams"),
-		createDefaultLogCategoryObject("requestheaders"),
-		createDefaultLogCategoryObject("trustedcas"),
-		createDefaultLogCategoryObject("restdatastore"),
-		createDefaultLogCategoryObject("policytree"),
-	})
+	_ datasource.DataSource              = &serverSettingsLogSettingsDataSource{}
+	_ datasource.DataSourceWithConfigure = &serverSettingsLogSettingsDataSource{}
 )
 
-func createDefaultLogCategoryObject(id string) types.Object {
-	defaultObj, _ := types.ObjectValue(logCategoriesAttrTypes, map[string]attr.Value{
-		"id":          types.StringValue(id),
-		"enabled":     types.BoolValue(false),
-		"name":        types.StringUnknown(),
-		"description": types.StringUnknown(),
-	})
-	return defaultObj
+// ServerSettingsLogSettingsDataSource is a helper function to simplify the provider implementation.
+func NewServerSettingsLogSettingsDataSource() datasource.DataSource {
+	return &serverSettingsLogSettingsDataSource{}
 }
 
-// ServerSettingsLogSettingsResource is a helper function to simplify the provider implementation.
-func ServerSettingsLogSettingsResource() resource.Resource {
-	return &serverSettingsLogSettingsResource{}
-}
-
-// serverSettingsLogSettingsResource is the resource implementation.
-type serverSettingsLogSettingsResource struct {
+// serverSettingsLogSettingsDataSource is the datasource implementation.
+type serverSettingsLogSettingsDataSource struct {
 	providerConfig internaltypes.ProviderConfiguration
 	apiClient      *client.APIClient
 }
 
-type serverSettingsLogSettingsResourceModel struct {
+type serverSettingsLogSettingsDataSourceModel struct {
 	Id            types.String `tfsdk:"id"`
 	LogCategories types.Set    `tfsdk:"log_categories"`
 }
 
-// GetSchema defines the schema for the resource.
-func (r *serverSettingsLogSettingsResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+// GetSchema defines the schema for the datasource.
+func (r *serverSettingsLogSettingsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	schema := schema.Schema{
-		Description: "LogSettings Settings related to server logging.",
+		Description: "LogSettings settings related to server logging.",
 		Attributes: map[string]schema.Attribute{
 			"log_categories": schema.SetNestedAttribute{
 				Description: "The log categories defined for the system and whether they are enabled. On a PUT request, if a category is not included in the list, it will be disabled.",
 				Optional:    true,
 				Computed:    true,
-				Default:     setdefault.StaticValue(logCategoriesDefault),
-				PlanModifiers: []planmodifier.Set{
-					setplanmodifier.UseStateForUnknown(),
-				},
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
-						"id": schema.StringAttribute{
-							Description: "The ID of the log category. This field must match one of the category IDs defined in log4j-categories.xml.",
-							Required:    true,
-						},
 						"name": schema.StringAttribute{
 							Description: "The description of the log category. This field is read-only and is ignored for PUT requests.",
-							Optional:    false,
 							Computed:    true,
-							// Adding these plan modifiers also seems to cause issues with Terraform's set planning logic. Possibly related to the issue linked below
-							/*PlanModifiers: []planmodifier.String{
-								stringplanmodifier.UseStateForUnknown(),
-							},*/
+							Optional:    false,
 						},
 						"description": schema.StringAttribute{
 							Description: "The description of the log category. This field is read-only and is ignored for PUT requests.",
-							Optional:    false,
 							Computed:    true,
-							// Adding these plan modifiers also seems to cause issues with Terraform's set planning logic. Possibly related to the issue linked below
-							/*PlanModifiers: []planmodifier.String{
-								stringplanmodifier.UseStateForUnknown(),
-							},*/
+							Optional:    false,
 						},
 						"enabled": schema.BoolAttribute{
 							Description: "Determines whether or not the log category is enabled. The default is false.",
-							Optional:    true,
 							Computed:    true,
-							// This default causes issues with unexpected plans - see https://github.com/hashicorp/terraform-plugin-framework/issues/867
-							// Default:     booldefault.StaticBool(false),
+							Optional:    false,
 						},
 					},
 				},
@@ -125,7 +75,7 @@ func (r *serverSettingsLogSettingsResource) Schema(ctx context.Context, req reso
 	resp.Schema = schema
 }
 
-func addOptionalServerSettingsLogSettingsFields(ctx context.Context, addRequest *client.LogSettings, plan serverSettingsLogSettingsResourceModel) error {
+func addOptionalServerSettingsLogSettingsFields(ctx context.Context, addRequest *client.LogSettings, plan serverSettingsLogSettingsDataSourceModel) error {
 	if internaltypes.IsDefined(plan.LogCategories) {
 		addRequest.LogCategories = []client.LogCategorySettings{}
 		for _, logCategoriesSetting := range plan.LogCategories.Elements() {
@@ -141,23 +91,23 @@ func addOptionalServerSettingsLogSettingsFields(ctx context.Context, addRequest 
 
 }
 
-// Metadata returns the resource type name.
-func (r *serverSettingsLogSettingsResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+// Metadata returns the datasource type name.
+func (r *serverSettingsLogSettingsDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = req.ProviderTypeName + "_server_settings_log_settings"
 }
 
-func (r *serverSettingsLogSettingsResource) Configure(_ context.Context, req resource.ConfigureRequest, _ *resource.ConfigureResponse) {
+func (r *serverSettingsLogSettingsDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
 	}
 
-	providerCfg := req.ProviderData.(internaltypes.ResourceConfiguration)
+	providerCfg := req.ProviderData.(internaltypes.DataSourceConfiguration)
 	r.providerConfig = providerCfg.ProviderConfig
 	r.apiClient = providerCfg.ApiClient
 
 }
 
-func readServerSettingsLogSettingsResponse(ctx context.Context, r *client.LogSettings, state *serverSettingsLogSettingsResourceModel, existingId *string) diag.Diagnostics {
+func readServerSettingsLogSettingsResponse(ctx context.Context, r *client.LogSettings, state *serverSettingsLogSettingsDataSourceModel, existingId *string) diag.Diagnostics {
 	var diags, respDiags diag.Diagnostics
 	state.Id = id.GenerateUUIDToState(existingId)
 	logCategorySettings := r.GetLogCategories()
@@ -179,40 +129,8 @@ func readServerSettingsLogSettingsResponse(ctx context.Context, r *client.LogSet
 	return diags
 }
 
-func (r *serverSettingsLogSettingsResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var plan serverSettingsLogSettingsResourceModel
-
-	diags := req.Plan.Get(ctx, &plan)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	createServerSettingsLogSettings := client.NewLogSettings()
-	err := addOptionalServerSettingsLogSettingsFields(ctx, createServerSettingsLogSettings, plan)
-	if err != nil {
-		resp.Diagnostics.AddError("Failed to add optional properties to add request for Server Settings Log Settings", err.Error())
-		return
-	}
-
-	apiCreateServerSettingsLogSettings := r.apiClient.ServerSettingsAPI.UpdateLogSettings(config.ProviderBasicAuthContext(ctx, r.providerConfig))
-	apiCreateServerSettingsLogSettings = apiCreateServerSettingsLogSettings.Body(*createServerSettingsLogSettings)
-	serverSettingsLogSettingsResponse, httpResp, err := r.apiClient.ServerSettingsAPI.UpdateLogSettingsExecute(apiCreateServerSettingsLogSettings)
-	if err != nil {
-		config.ReportHttpError(ctx, &resp.Diagnostics, "An error occurred while creating the Server Settings Log Settings", err, httpResp)
-		return
-	}
-
-	// Read the response into the state
-	var state serverSettingsLogSettingsResourceModel
-	diags = readServerSettingsLogSettingsResponse(ctx, serverSettingsLogSettingsResponse, &state, nil)
-	resp.Diagnostics.Append(diags...)
-	diags = resp.State.Set(ctx, state)
-	resp.Diagnostics.Append(diags...)
-}
-
-func (r *serverSettingsLogSettingsResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var state serverSettingsLogSettingsResourceModel
+func (r *serverSettingsLogSettingsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state serverSettingsLogSettingsDataSourceModel
 
 	diags := req.State.Get(ctx, &state)
 	resp.Diagnostics.Append(diags...)
@@ -223,7 +141,7 @@ func (r *serverSettingsLogSettingsResource) Read(ctx context.Context, req resour
 	if err != nil {
 		if httpResp != nil && httpResp.StatusCode == 404 {
 			config.ReportHttpErrorAsWarning(ctx, &resp.Diagnostics, "An error occurred while getting the Server Settings Log Settings", err, httpResp)
-			resp.State.RemoveResource(ctx)
+			resp.State.RemoveDataSource(ctx)
 		} else {
 			config.ReportHttpError(ctx, &resp.Diagnostics, "An error occurred while getting the Server Settings Log Settings", err, httpResp)
 		}
@@ -244,51 +162,7 @@ func (r *serverSettingsLogSettingsResource) Read(ctx context.Context, req resour
 	resp.Diagnostics.Append(diags...)
 }
 
-// Update updates the resource and sets the updated Terraform state on success.
-func (r *serverSettingsLogSettingsResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	// Retrieve values from plan
-	var plan serverSettingsLogSettingsResourceModel
-	diags := req.Plan.Get(ctx, &plan)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	updateServerSettingsLogSettings := r.apiClient.ServerSettingsAPI.UpdateLogSettings(config.ProviderBasicAuthContext(ctx, r.providerConfig))
-	createUpdateRequest := client.NewLogSettings()
-	err := addOptionalServerSettingsLogSettingsFields(ctx, createUpdateRequest, plan)
-	if err != nil {
-		resp.Diagnostics.AddError("Failed to add optional properties to add request for Server Settings Log Settings", err.Error())
-		return
-	}
-
-	updateServerSettingsLogSettings = updateServerSettingsLogSettings.Body(*createUpdateRequest)
-	updateServerSettingsLogSettingsResponse, httpResp, err := r.apiClient.ServerSettingsAPI.UpdateLogSettingsExecute(updateServerSettingsLogSettings)
-	if err != nil {
-		config.ReportHttpError(ctx, &resp.Diagnostics, "An error occurred while updating Server Settings Log Settings", err, httpResp)
-		return
-	}
-
-	// Read the response
-	var state serverSettingsLogSettingsResourceModel
-	id, diags := id.GetID(ctx, req.State)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	diags = readServerSettingsLogSettingsResponse(ctx, updateServerSettingsLogSettingsResponse, &state, id)
-	resp.Diagnostics.Append(diags...)
-
-	// Update computed values
-	diags = resp.State.Set(ctx, state)
-	resp.Diagnostics.Append(diags...)
-}
-
-// This config object is edit-only, so Terraform can't delete it.
-func (r *serverSettingsLogSettingsResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-}
-
-func (r *serverSettingsLogSettingsResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+func (r *serverSettingsLogSettingsDataSource) ImportState(ctx context.Context, req datasource.ImportStateRequest, resp *datasource.ImportStateResponse) {
 	// Retrieve import ID and save to id attribute
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	datasource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
