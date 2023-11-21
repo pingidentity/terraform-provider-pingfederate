c06844cb0df41fc1b9b20db7f2106d96f70dee0c
PingDavidR <davidross@pingidentity.com>
2023-11-21 13:42:53 -0600

    add session-settings data source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -318,6 +318,7 @@ func (p *pingfederateProvider) DataSources(_ context.Context) []func() datasourc
 		serversettingslogsettings.NewServerSettingsLogSettingsDataSource,
 		serversettingssystemkeys.NewServerSettingsSystemKeysDataSource,
 		sessionapplicationsessionpolicy.NewSessionApplicationSessionPolicyDataSource,
+		sessionsettings.NewSessionSettingsDataSource,
 		virtualhostnames.NewVirtualHostNamesDataSource,
 	}
 }
