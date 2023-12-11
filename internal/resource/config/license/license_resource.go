b4d09ee8a0633849e600f9fa89552a5b180df822
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2023-12-11 14:44:59 -0600

    Add computed fields to keys and license resources (#138)
    * Add computed fields to key_pair_signing_import_resource
    
    * Add computed fields to key pair ssl server import resource
    
    * Add computed attributes to license resource
    
    * Update changelog
    
    * Generate docs
    
    * Add missing parentheses

--- internal/resource/config/license/license_resource.go
+++ internal/resource/config/license/license_resource.go
@@ -2,7 +2,10 @@ package license
 
 import (
 	"context"
+	"time"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -19,6 +22,18 @@ import (
 var (
 	_ resource.Resource              = &licenseResource{}
 	_ resource.ResourceWithConfigure = &licenseResource{}
+
+	licenseGroupsAttrTypes = map[string]attr.Type{
+		"name":             types.StringType,
+		"connection_count": types.Int64Type,
+		"start_date":       types.StringType,
+		"end_date":         types.StringType,
+	}
+
+	featuresAttrTypes = map[string]attr.Type{
+		"name":  types.StringType,
+		"value": types.StringType,
+	}
 )
 
 // LicenseResource is a helper function to simplify the provider implementation.
@@ -35,6 +50,25 @@ type licenseResource struct {
 type licenseResourceModel struct {
 	Id       types.String `tfsdk:"id"`
 	FileData types.String `tfsdk:"file_data"`
+	// Computed attributes
+	Name                types.String `tfsdk:"name"`
+	MaxConnections      types.Int64  `tfsdk:"max_connections"`
+	UsedConnections     types.Int64  `tfsdk:"used_connections"`
+	Tier                types.String `tfsdk:"tier"`
+	IssueDate           types.String `tfsdk:"issue_date"`
+	ExpirationDate      types.String `tfsdk:"expiration_date"`
+	EnforcementType     types.String `tfsdk:"enforcement_type"`
+	Version             types.String `tfsdk:"version"`
+	Product             types.String `tfsdk:"product"`
+	Organization        types.String `tfsdk:"organization"`
+	GracePeriod         types.Int64  `tfsdk:"grace_period"`
+	NodeLimit           types.Int64  `tfsdk:"node_limit"`
+	LicenseGroups       types.List   `tfsdk:"license_groups"`
+	OauthEnabled        types.Bool   `tfsdk:"oauth_enabled"`
+	WsTrustEnabled      types.Bool   `tfsdk:"ws_trust_enabled"`
+	ProvisioningEnabled types.Bool   `tfsdk:"provisioning_enabled"`
+	BridgeMode          types.Bool   `tfsdk:"bridge_mode"`
+	Features            types.List   `tfsdk:"features"`
 }
 
 // GetSchema defines the schema for the resource.
@@ -46,6 +80,158 @@ func (r *licenseResource) Schema(ctx context.Context, req resource.SchemaRequest
 				Required: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"name": schema.StringAttribute{
+				Description: "Name of the person the license was issued to.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"max_connections": schema.Int64Attribute{
+				Description: "Maximum number of connections that can be created under this license (if applicable).",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"used_connections": schema.Int64Attribute{
+				Description: "Number of used connections under this license.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"tier": schema.StringAttribute{
+				Description: "The tier value from the license file. The possible values are FREE, PERPETUAL or SUBSCRIPTION.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"issue_date": schema.StringAttribute{
+				Description: "The issue date value from the license file.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"expiration_date": schema.StringAttribute{
+				Description: "The expiration date value from the license file (if applicable).",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"enforcement_type": schema.StringAttribute{
+				Description: "The enforcement type is a 3-bit binary value, expressed as a decimal digit. The bits from left to right are: 1: Shutdown on expire. 2: Notify on expire. 4: Enforce minor version. if all three enforcements are active, the enforcement type will be 7 (1 + 2 + 4); if only the first two are active, you have an enforcement type of 3 (1 + 2).",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"version": schema.StringAttribute{
+				Description: "The Ping Identity product version from the license file.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"product": schema.StringAttribute{
+				Description: "The Ping Identity product value from the license file.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"organization": schema.StringAttribute{
+				Description: "The organization value from the license file.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"grace_period": schema.Int64Attribute{
+				Description: "Number of days provided as grace period, past the expiration date (if applicable).",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"node_limit": schema.Int64Attribute{
+				Description: "Maximum number of clustered nodes allowed under this license (if applicable).",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"license_groups": schema.ListNestedAttribute{
+				Description: "License connection groups, if applicable.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"name": schema.StringAttribute{
+							Description: "Group name from the license file.",
+							Required:    false,
+							Optional:    false,
+							Computed:    true,
+						},
+						"connection_count": schema.Int64Attribute{
+							Description: "Maximum number of connections permitted under the group.",
+							Required:    false,
+							Optional:    false,
+							Computed:    true,
+						},
+						"start_date": schema.StringAttribute{
+							Description: "Start date for the group.",
+							Required:    false,
+							Optional:    false,
+							Computed:    true,
+						},
+						"end_date": schema.StringAttribute{
+							Description: "End date for the group.",
+							Required:    false,
+							Optional:    false,
+							Computed:    true,
+						},
+					},
+				},
+			},
+			"oauth_enabled": schema.BoolAttribute{
+				Description: "Indicates whether OAuth role is enabled for this license.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"ws_trust_enabled": schema.BoolAttribute{
+				Description: "Indicates whether WS-Trust role is enabled for this license.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"provisioning_enabled": schema.BoolAttribute{
+				Description: "Indicates whether Provisioning role is enabled for this license.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"bridge_mode": schema.BoolAttribute{
+				Description: "Indicates whether this license is a bridge license or not.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+			},
+			"features": schema.ListNestedAttribute{
+				Description: "Other licence features, if applicable.",
+				Required:    false,
+				Optional:    false,
+				Computed:    true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"name": schema.StringAttribute{
+							Description: "The name of the license feature.",
+							Required:    false,
+							Optional:    false,
+							Computed:    true,
+						},
+						"value": schema.StringAttribute{
+							Description: "The value of the license feature.",
+							Required:    false,
+							Optional:    false,
+							Computed:    true,
+						},
+					},
 				},
 			},
 		},
@@ -70,9 +256,34 @@ func (r *licenseResource) Configure(_ context.Context, req resource.ConfigureReq
 
 }
 
-func readLicenseResponse(ctx context.Context, r *client.LicenseView, state *licenseResourceModel, expectedValues *licenseResourceModel, planFileData types.String, existingId *string) {
+func readLicenseResponse(ctx context.Context, r *client.LicenseView, state *licenseResourceModel, planFileData types.String, existingId *string) diag.Diagnostics {
+	var diags, respDiags diag.Diagnostics
 	state.Id = id.GenerateUUIDToState(existingId)
 	state.FileData = types.StringValue(planFileData.ValueString())
+
+	state.Name = types.StringPointerValue(r.Name)
+	state.MaxConnections = types.Int64PointerValue(r.MaxConnections)
+	state.UsedConnections = types.Int64PointerValue(r.UsedConnections)
+	state.Tier = types.StringPointerValue(r.Tier)
+	state.IssueDate = types.StringValue(r.IssueDate.Format(time.RFC3339))
+	state.ExpirationDate = types.StringValue(r.ExpirationDate.Format(time.RFC3339))
+	state.EnforcementType = types.StringPointerValue(r.EnforcementType)
+	state.Version = types.StringPointerValue(r.Version)
+	state.Product = types.StringPointerValue(r.Product)
+	state.Organization = types.StringPointerValue(r.Organization)
+	state.GracePeriod = types.Int64PointerValue(r.GracePeriod)
+	state.NodeLimit = types.Int64PointerValue(r.NodeLimit)
+	state.OauthEnabled = types.BoolValue(*r.OauthEnabled)
+	state.WsTrustEnabled = types.BoolValue(*r.WsTrustEnabled)
+	state.ProvisioningEnabled = types.BoolValue(*r.ProvisioningEnabled)
+	state.BridgeMode = types.BoolValue(*r.BridgeMode)
+
+	state.LicenseGroups, respDiags = types.ListValueFrom(ctx, types.ObjectType{AttrTypes: licenseGroupsAttrTypes}, r.LicenseGroups)
+	diags.Append(respDiags...)
+
+	state.Features, respDiags = types.ListValueFrom(ctx, types.ObjectType{AttrTypes: featuresAttrTypes}, r.Features)
+	diags.Append(respDiags...)
+	return diags
 }
 
 func (r *licenseResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
@@ -95,7 +306,8 @@ func (r *licenseResource) Create(ctx context.Context, req resource.CreateRequest
 
 	// Read the response into the state
 	var state licenseResourceModel
-	readLicenseResponse(ctx, licenseResponse, &state, &state, plan.FileData, nil)
+	diags = readLicenseResponse(ctx, licenseResponse, &state, plan.FileData, nil)
+	resp.Diagnostics.Append(diags...)
 
 	diags = resp.State.Set(ctx, state)
 	resp.Diagnostics.Append(diags...)
@@ -126,7 +338,8 @@ func (r *licenseResource) Read(ctx context.Context, req resource.ReadRequest, re
 	if resp.Diagnostics.HasError() {
 		return
 	}
-	readLicenseResponse(ctx, apiReadLicense, &state, &state, state.FileData, id)
+	diags = readLicenseResponse(ctx, apiReadLicense, &state, state.FileData, id)
+	resp.Diagnostics.Append(diags...)
 
 	// Set refreshed state
 	diags = resp.State.Set(ctx, &state)
@@ -159,7 +372,8 @@ func (r *licenseResource) Update(ctx context.Context, req resource.UpdateRequest
 		return
 	}
 	var state licenseResourceModel
-	readLicenseResponse(ctx, updateLicenseResponse, &state, &state, plan.FileData, id)
+	diags = readLicenseResponse(ctx, updateLicenseResponse, &state, plan.FileData, id)
+	resp.Diagnostics.Append(diags...)
 
 	// Update computed values
 	diags = resp.State.Set(ctx, state)
