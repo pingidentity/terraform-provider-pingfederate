4291cad1dc1eb54f55d60900fe29a2243aea391b
Henry Recker <henryrecker@pingidentity.com>
2023-12-12 11:01:39 -0600

    Remove old utility methods from merge

--- internal/resource/config/administrativeaccount/administrative_account_common.go
+++ internal/resource/config/administrativeaccount/administrative_account_common.go
@@ -39,10 +39,10 @@ func readAdministrativeAccountResponse(ctx context.Context, r *client.Administra
 		state.EncryptedPassword = types.StringPointerValue(r.EncryptedPassword)
 	}
 	state.Active = types.BoolPointerValue(r.Active)
-	state.Description = internaltypes.StringTypeOrNil(r.Description, false)
+	state.Description = types.StringPointerValue(r.Description)
 	state.Auditor = types.BoolPointerValue(r.Auditor)
-	state.PhoneNumber = internaltypes.StringTypeOrNil(r.PhoneNumber, false)
-	state.EmailAddress = internaltypes.StringTypeOrNil(r.EmailAddress, false)
-	state.Department = internaltypes.StringTypeOrNil(r.Department, false)
+	state.PhoneNumber = types.StringPointerValue(r.PhoneNumber)
+	state.EmailAddress = types.StringPointerValue(r.EmailAddress)
+	state.Department = types.StringPointerValue(r.Department)
 	state.Roles = internaltypes.GetStringSet(r.Roles)
 }
