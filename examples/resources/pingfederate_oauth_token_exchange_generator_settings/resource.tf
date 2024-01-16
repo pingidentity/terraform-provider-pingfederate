e56d9495fa7fbdcacd0b9eac649c9a3acde69334
Wes <71149180+wesleymccollam@users.noreply.github.com>
2024-01-16 13:29:44 -0500

    oauth tokenExchange generator settings resource (#163)

---
+++ examples/resources/pingfederate_oauth_token_exchange_generator_settings/resource.tf
@@ -0,0 +1,5 @@
+resource "pingfederate_oauth_token_exchange_generator_settings" "oauthTokenExchangeGeneratorSettingsExample" {
+  default_generator_group_ref = {
+    id = "myTokenExchangeGeneratorGroup"
+  }
+}
