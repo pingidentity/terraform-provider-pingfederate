d21478b3295eb9e13d77fb847065465c07650412
wesleymccollam <wesleymccollam@pingidentity.com>
2024-02-09 13:59:18 -0500

    remove insecure_trust_all_tls and x_bypass_external_validation_header vars from auth examples

--- examples/doc-examples/basic-auth.tf
+++ examples/doc-examples/basic-auth.tf
@@ -1,9 +1,7 @@
 provider "pingfederate" {
-  username                            = "administrator"
-  password                            = "2FederateM0re"
-  https_host                          = "https://localhost:9999"
-  admin_api_path                      = "/pf-admin-api/v1"
-  insecure_trust_all_tls              = true
-  x_bypass_external_validation_header = true
-  product_version                     = "12.0"
+  username        = "administrator"
+  password        = "2FederateM0re"
+  https_host      = "https://localhost:9999"
+  admin_api_path  = "/pf-admin-api/v1"
+  product_version = "12.0"
 }
--- examples/doc-examples/oauth2.tf
+++ examples/doc-examples/oauth2.tf
@@ -1,11 +1,9 @@
 provider "pingfederate" {
-  client_id                           = "clientid"
-  client_secret                       = "clientsecret"
-  scopes                              = ["scope"]
-  token_url                           = "https://localhost:9031/as/token.oauth2"
-  https_host                          = "https://localhost:9999"
-  admin_api_path                      = "/pf-admin-api/v1"
-  insecure_trust_all_tls              = true
-  x_bypass_external_validation_header = true
-  product_version                     = "12.0"
+  client_id       = "clientid"
+  client_secret   = "clientsecret"
+  scopes          = ["scope"]
+  token_url       = "https://localhost:9031/as/token.oauth2"
+  https_host      = "https://localhost:9999"
+  admin_api_path  = "/pf-admin-api/v1"
+  product_version = "12.0"
 }
