ba00e6189ec710d72a172d1e91ea94a352c4f5b9
PingDavidR <davidross@pingidentity.com>
2024-02-14 12:53:09 -0600

    newline

--- examples/resources/pingfederate_incoming_proxy_settings/resource.tf
+++ examples/resources/pingfederate_incoming_proxy_settings/resource.tf
@@ -1,7 +1,9 @@
 resource "pingfederate_incoming_proxy_settings" "incomingProxySettingsExample" {
   forwarded_ip_address_header_name  = "X-Forwarded-For"
+  forwarded_ip_address_header_index = "FIRST"
   forwarded_host_header_name        = "X-Forwarded-Host"
   forwarded_host_header_index       = "LAST"
-
-  proxy_terminates_https_conns      = false
+  client_cert_ssl_header_name       = "X-Client-Cert"
+  client_cert_chain_ssl_header_name = "X-Client-Cert-Chain"
+  proxy_terminates_https_conns      = true
 }
