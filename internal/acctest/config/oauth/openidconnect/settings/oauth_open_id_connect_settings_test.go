1bc99c0445bf7f41a6e4fd490dbc2a47479e2dbc
PingDavidR <davidross@pingidentity.com>
2024-02-26 11:33:40 -0600

    cleanup error logging and comments

--- internal/acctest/config/oauth/openidconnect/settings/oauth_open_id_connect_settings_test.go
+++ internal/acctest/config/oauth/openidconnect/settings/oauth_open_id_connect_settings_test.go
@@ -19,16 +19,6 @@ type openIdConnectSettingsResourceModel struct {
 	sessionSettings  *client.OIDCSessionSettings
 }
 
-// This function is used to delete the server-profile provided OAuth client at the start of the tests
-// func deleteOauthClient(t *testing.T) {
-// 	testClient := acctest.TestClient()
-// 	ctx := acctest.TestBasicAuthContext()
-// 	_, err := testClient.OauthClientsAPI.DeleteOauthClient(ctx, "test").Execute()
-// 	if err != nil {
-// 		t.Fatalf("Failed to delete conflicting \"test\" OAuth Client: %v", err)
-// 	}
-// }
-
 func createDependenciesForTestBecauseIHaveTo(t *testing.T) {
 	testClient := acctest.TestClient()
 	ctx := acctest.TestBasicAuthContext()
@@ -79,26 +69,11 @@ func createDependenciesForTestBecauseIHaveTo(t *testing.T) {
 func deleteOidcPolicy(t *testing.T) {
 	testClient := acctest.TestClient()
 	ctx := acctest.TestBasicAuthContext()
-	resp, err := testClient.OauthOpenIdConnectAPI.DeleteOIDCPolicy(ctx, "oidcPolicy").Execute()
-	fmt.Print(resp)
+	_, err := testClient.OauthOpenIdConnectAPI.DeleteOIDCPolicy(ctx, "oidcPolicy").Execute()
 	if err != nil {
 		t.Fatalf("Failed to delete OIDC Policy: %v", err)
 	}
 }
-
-// // This function is used to add back the OAuth client for remaining tests
-// func recreateOauthClient(t *testing.T) {
-// 	testClient := acctest.TestClient()
-// 	ctx := acctest.TestBasicAuthContext()
-
-// 	replacementOauthClient := client.NewClient()
-// 	oauthClient.ClientId = "test"
-// 	//_, _, err := testClient.OauthClientsAPI.CreateOauthClient()
-
-// 	if err != nil {
-// 		t.Fatalf("Failed to recreate \"test\" OAuth Client: %v", err)
-// 	}
-// }
 
 func TestAccOpenIdConnectSettings(t *testing.T) {
 	resourceName := "myOpenIdConnectSettings"
