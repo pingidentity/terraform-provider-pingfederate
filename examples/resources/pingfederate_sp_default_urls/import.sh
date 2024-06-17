8599ad5b81823cce6947be5d9670afdff409fc0d
Henry Recker <henryrecker@pingidentity.com>
2024-06-17 11:07:27 -0500

    Move import note to doc template

--- examples/resources/pingfederate_sp_default_urls/import.sh
+++ examples/resources/pingfederate_sp_default_urls/import.sh
@@ -1,2 +1,1 @@
-# This resource is singleton, so the value of "id" doesn't matter - it is just a placeholder, and required by Terraform
 terraform import pingfederate_sp_default_urls.spDefaultUrlsExample id
