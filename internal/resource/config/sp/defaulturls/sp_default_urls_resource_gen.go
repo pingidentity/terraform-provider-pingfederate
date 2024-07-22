9e078ab323185a1bb0f4c9fcb6a7519357b2f27c
Henry Recker <henryrecker@pingidentity.com>
2024-07-22 14:50:13 -0500

    Update PF client version

--- internal/resource/config/sp/defaulturls/sp_default_urls_resource_gen.go
+++ internal/resource/config/sp/defaulturls/sp_default_urls_resource_gen.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	client "github.com/pingidentity/pingfederate-go-client/v1200/configurationapi"
+	client "github.com/pingidentity/pingfederate-go-client/v1210/configurationapi"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/configvalidators"
 	internaltypes "github.com/pingidentity/terraform-provider-pingfederate/internal/types"
