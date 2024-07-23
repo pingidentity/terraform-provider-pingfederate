e22696f5c793cae5ea5bd6a84b86a6bbc954274f
Henry Recker <henryrecker@pingidentity.com>
2024-07-23 14:02:27 -0500

    Update client version

--- internal/resource/config/kerberos/realms/settings/kerberos_realm_settings_resource_gen.go
+++ internal/resource/config/kerberos/realms/settings/kerberos_realm_settings_resource_gen.go
@@ -11,7 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	client "github.com/pingidentity/pingfederate-go-client/v1200/configurationapi"
+	client "github.com/pingidentity/pingfederate-go-client/v1210/configurationapi"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config"
 	internaltypes "github.com/pingidentity/terraform-provider-pingfederate/internal/types"
 )
