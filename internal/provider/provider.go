712d93eb43abacc096dd9efd473bf65861651caa
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2024-07-31 09:55:46 -0500

    pingfederate_sp_default_urls resource (#260)
    * pingfederate_sp_default_urls resource
    
    Also added new testaccfolder for faster testing of individual acceptance test files
    
    * Move import note to doc template
    
    * Add resource description and format default value
    
    * Add validation for URLs, and update URL validator to use url library for parsing
    
    * Update PF client version
    
    * Remove duplicate target from Makefile
    
    * Reset resource on delete rather than just removing from terraform state
    
    * Update to a single merge default_urls resource for both idp and sp default urls, and fix UAT feedback
    
    * Fix typo

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -74,6 +74,7 @@ import (
 	sessionauthenticationsessionpoliciesglobal "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/session/authenticationsessionpolicies/global"
 	sessionsettings "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/session/settings"
 	spauthenticationpolicycontractmapping "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/sp/authenticationpolicycontractmapping"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/sp/defaulturls"
 	sptargeturlmappings "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/sp/targeturlmappings"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/tokenprocessortotokengeneratormapping"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/virtualhostnames"
@@ -719,6 +720,7 @@ func (p *pingfederateProvider) Resources(_ context.Context) []func() resource.Re
 		authenticationpolicycontract.AuthenticationPolicyContractResource,
 		authenticationselector.AuthenticationSelectorsResource,
 		certificate.CertificateCAResource,
+		defaulturls.DefaultUrlsResource,
 		extendedproperties.ExtendedPropertiesResource,
 		idpadapter.IdpAdapterResource,
 		idpdefaulturls.IdpDefaultUrlsResource,
