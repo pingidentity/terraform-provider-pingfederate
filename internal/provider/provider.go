98473049b2d7408a191e71231456a6c766e75542
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2024-08-07 12:14:29 -0500

    pingfederate_keypairs_oauth_openid_connect_additional_key_set resource (#271)
    * pingfederate_keypairs_oauth_openid_connect_additional_key_set resource
    
    * Add resource description, fix attribute descriptions
    
    * Add length validator, computed, and default to description
    
    * Add PF id validator and generate doc
    
    * UAT fixes, and updating 2 digit version to default to latest patch for validation
    
    * Fix for validation
    
    * Fix version error messages and indicate required provider attrs in docs
    
    * Update 404 warning

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -42,6 +42,7 @@ import (
 	incomingproxysettings "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/incomingproxysettings"
 	kerberosrealms "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/kerberos/realms"
 	kerberosrealmssettings "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/kerberos/realms/settings"
+	keypairsoauthopenidconnectadditionalkeysets "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/keypairs/oauthopenidconnect/additionalkeysets"
 	keypairsigningimport "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/keypairs/signing/import"
 	keypairsslserverimport "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/keypairs/sslserver/import"
 	keypairssslserversettings "github.com/pingidentity/terraform-provider-pingfederate/internal/resource/config/keypairs/sslserver/settings"
@@ -734,6 +735,7 @@ func (p *pingfederateProvider) Resources(_ context.Context) []func() resource.Re
 		incomingproxysettings.IncomingProxySettingsResource,
 		kerberosrealms.KerberosRealmsResource,
 		kerberosrealmssettings.KerberosRealmSettingsResource,
+		keypairsoauthopenidconnectadditionalkeysets.KeypairsOauthOpenidConnectAdditionalKeySetResource,
 		keypairsigningimport.KeyPairsSigningImportResource,
 		keypairsslserverimport.KeyPairsSslServerImportResource,
 		keypairssslserversettings.KeypairsSslServerSettingsResource,
