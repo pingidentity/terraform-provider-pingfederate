56253d6c051254a4bfa11465cea425ec708da8d3
Henry Recker <henryrecker@pingidentity.com>
2024-08-26 11:41:35 -0500

    Doc fixes for server_settings_system_keys

--- examples/resources/pingfederate_server_settings_system_keys/resource.tf
+++ examples/resources/pingfederate_server_settings_system_keys/resource.tf
@@ -1,8 +1,8 @@
-resource "pingfederate_server_settings_system_keys" "serverSettingsSystemKeysExample" {
+resource "pingfederate_server_settings_system_keys" "systemKeys" {
   current = {
-    encrypted_key_data = ""
+    encrypted_key_data = var.current_server_encrypted_key_data
   }
   pending = {
-    encrypted_key_data = ""
+    encrypted_key_data = var.pending_server_encrypted_key_data
   }
 }
