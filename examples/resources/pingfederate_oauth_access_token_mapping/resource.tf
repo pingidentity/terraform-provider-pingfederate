8902deb8322612f67802e22c4163e7230598f4f3
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2024-09-16 15:29:17 +0100

    Update OAuth ATM Mapping HCL example (#384)

--- examples/resources/pingfederate_oauth_access_token_mapping/resource.tf
+++ examples/resources/pingfederate_oauth_access_token_mapping/resource.tf
@@ -1,214 +1,107 @@
-resource "pingfederate_oauth_access_token_manager" "jwt_example" {
-  manager_id = "jsonWebTokenOatm"
-  name       = "jsonWebTokenExample"
+resource "pingfederate_oauth_access_token_manager" "devices" {
+  manager_id = "devicesATM"
+  name       = "Device Token Manager"
+
   plugin_descriptor_ref = {
-    id = "com.pingidentity.pf.access.token.management.plugins.JwtBearerAccessTokenManagementPlugin"
+    id = "org.sourceid.oauth20.token.plugin.impl.ReferenceBearerAccessTokenManagementPlugin"
   }
+
   configuration = {
-    tables = [
-      {
-        name = "Symmetric Keys"
-        rows = [
-          {
-            fields = [
-              {
-                name  = "Key ID"
-                value = "keyidentifier"
-              },
-              {
-                name  = "Encoding"
-                value = "b64u"
-              }
-            ]
-            sensitive_fields = [
-              {
-                name  = "Key"
-                value = var.access_token_manager_key
-              }
-            ]
-            default_row = false
-          }
-        ]
-      },
-      {
-        name = "Certificates"
-        rows = []
-      }
-    ]
     fields = [
       {
+        name  = "Token Length"
+        value = "56"
+      },
+      {
         name  = "Token Lifetime"
-        value = "120"
+        value = "240"
       },
-      {
-        name  = "Use Centralized Signing Key"
-        value = "false"
-      },
-      {
-        name  = "JWS Algorithm"
-        value = ""
-      },
-      {
-        name  = "Active Symmetric Key ID"
-        value = "keyidentifier"
-      },
-      {
-        name  = "Active Signing Certificate Key ID"
-        value = ""
-      },
-      {
-        name  = "JWE Algorithm"
-        value = "dir"
-      },
-      {
-        name  = "JWE Content Encryption Algorithm"
-        value = "A192CBC-HS384"
-      },
-      {
-        name  = "Active Symmetric Encryption Key ID"
-        value = "keyidentifier"
-      },
-      {
-        name  = "Asymmetric Encryption Key"
-        value = ""
-      },
-      {
-        name  = "Asymmetric Encryption JWKS URL"
-        value = ""
-      },
-      {
-        name  = "Enable Token Revocation"
-        value = "false"
-      },
-      {
-        name  = "Include Key ID Header Parameter"
-        value = "true"
-      },
-      {
-        name  = "Default JWKS URL Cache Duration"
-        value = "720"
-      },
-      {
-        name  = "Include JWE Key ID Header Parameter"
-        value = "true"
-      },
-      {
-        name  = "Client ID Claim Name"
-        value = "client_id"
-      },
-      {
-        name  = "Scope Claim Name"
-        value = "scope"
-      },
-      {
-        name  = "Space Delimit Scope Values"
-        value = "true"
-      },
-      {
-        name  = "Authorization Details Claim Name"
-        value = "authorization_details"
-      },
-      {
-        name  = "Issuer Claim Value"
-        value = ""
-      },
-      {
-        name  = "Audience Claim Value"
-        value = ""
-      },
-      {
-        name  = "JWT ID Claim Length"
-        value = "22"
-      },
-      {
-        name  = "Access Grant GUID Claim Name"
-        value = ""
-      },
-      {
-        name  = "JWKS Endpoint Path"
-        value = ""
-      },
-      {
-        name  = "JWKS Endpoint Cache Duration"
-        value = "720"
-      },
-      {
-        name  = "Expand Scope Groups"
-        value = "false"
-      },
-      {
-        name  = "Type Header Value"
-        value = ""
-      }
     ]
   }
   attribute_contract = {
     extended_attributes = [
-      {
-        name         = "contract"
-        multi_valued = false
-      }
+      { name = "directory_id" },
+      { name = "device_id" },
+      { name = "device_owner_user_id" },
+      { name = "device_custodian_user_id" },
     ]
-  }
-  selection_settings = {
-    resource_uris = []
-  }
-  access_control_settings = {
-    restrict_clients = false
-  }
-  session_validation_settings = {
-    check_valid_authn_session       = false
-    check_session_revocation_status = false
-    update_authn_session_activity   = false
-    include_session_id              = false
   }
 }
 
-resource "pingfederate_oauth_access_token_mapping" "oauthAccessTokenMappingExample" {
+resource "pingfederate_oauth_access_token_mapping" "example_device" {
+  context = {
+    type = "DEFAULT"
+  }
+
   access_token_manager_ref = {
-    id = pingfederate_oauth_access_token_manager.jwt_example.manager_id
+    id = pingfederate_oauth_access_token_manager.devices.id
   }
-  attribute_contract_fulfillment = {
-    "extended_contract" = {
-      source = {
-        type = "TEXT"
-      }
-      value = "Administrator"
-    },
-  }
+
   attribute_sources = [
     {
-      jdbc_attribute_source = {
-        column_names = [
-          "GRANTEE",
-          "IS_GRANTABLE",
-          "ROLE_NAME",
-        ]
+      ldap_attribute_source = {
+        base_dn = "ou=Devices,dc=bxretail,dc=org"
         data_store_ref = {
-          id = "ProvisionerDS"
+          id = pingfederate_data_store.ldap_data_store.id
         }
-        description = "description"
-        filter      = "$${client_id}"
-        id          = "test"
-        schema      = "INFORMATION_SCHEMA"
-        table       = "ADMINISTRABLE_ROLE_AUTHORIZATIONS"
+        description            = "Directory"
+        id                     = "Directory"
+        member_of_nested_group = false
+        search_attributes = [
+          "Subject DN",
+          "serialNumber",
+          "owner",
+          "seeAlso",
+          "uid",
+        ]
+        search_filter = "cn=$${USER_KEY}"
+        search_scope  = "SUBTREE"
+        type          = "LDAP"
       }
     },
   ]
-  context = {
-    type = "CLIENT_CREDENTIALS"
+
+  attribute_contract_fulfillment = {
+    "directory_id" = {
+      source = {
+        id   = "Directory"
+        type = "LDAP_DATA_STORE"
+      }
+      value = "uid"
+    },
+    "device_id" = {
+      source = {
+        id   = "Directory"
+        type = "LDAP_DATA_STORE"
+      }
+      value = "serialNumber"
+    },
+    "device_owner_user_id" = {
+      source = {
+        id   = "Directory"
+        type = "LDAP_DATA_STORE"
+      }
+      value = "owner"
+    },
+    "device_custodian_user_id" = {
+      source = {
+        id   = "Directory"
+        type = "LDAP_DATA_STORE"
+      }
+      value = "seeAlso"
+    },
   }
 
   issuance_criteria = {
     conditional_criteria = [
       {
-        attribute_name = "ClientId"
-        condition      = "EQUALS_CASE_INSENSITIVE"
-        error_result   = "error"
+        attribute_name = "Device Type"
+        condition      = "NOT_EQUAL_CASE_INSENSITIVE"
+        error_result   = "Cannot issue access token"
         source = {
-          type = "CONTEXT"
+          type = "EXTENDED_PROPERTIES"
         }
-        value = "text"
+        value = "User Device"
       },
     ]
   }
