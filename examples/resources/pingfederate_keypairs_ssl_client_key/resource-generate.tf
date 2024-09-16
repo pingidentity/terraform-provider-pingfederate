059e3510ac0549b4a513696b78fe63b4d8bb84d6
Patrick Cowland <patrickcowland@pingidentity.com>
2024-09-16 15:04:50 +0100

    Update doc HCL example

--- examples/resources/pingfederate_keypairs_ssl_client_key/resource-generate.tf
+++ examples/resources/pingfederate_keypairs_ssl_client_key/resource-generate.tf
@@ -1,14 +1,14 @@
 resource "pingfederate_keypairs_ssl_client_key" "sslClientKey" {
   key_id                    = "sslclientkey"
   city                      = "Austin"
-  common_name               = "Example"
+  common_name               = "example"
   country                   = "US"
   key_algorithm             = "RSA"
   key_size                  = 2048
-  organization              = "Ping Identity"
-  organization_unit         = "Engineering"
+  organization              = "BXRetail"
+  organization_unit         = "Auth Services"
   signature_algorithm       = "SHA256withRSA"
   state                     = "Texas"
-  subject_alternative_names = ["example.com"]
+  subject_alternative_names = ["bxretail.org"]
   valid_days                = 365
 }
