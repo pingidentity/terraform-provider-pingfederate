c0e23b595a64b0b63e6f44eb9ad7ac8c444624a1
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2024-09-17 13:51:35 -0500

    pingfederate_idp_token_processor resource (#277)
    * pingfederate_idp_token_processor resource
    
    * Add resource description and update attribute descriptions
    
    * Add validation for string length
    
    * Update 404 warning message and generate doc
    
    * Add id attr and import to non-read-only configuration
    
    * Update HCL example
    
    * Update error messages
    
    * Update sets to lists
    
    * Handle drifts in table content in tables_all
    
    ---------
    
    Co-authored-by: Patrick Cowland <patrickcowland@pingidentity.com>

---
+++ internal/resource/config/idp/tokenprocessors/idp_token_processor_resource.go
@@ -0,0 +1,16 @@
+package idptokenprocessors
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	attributeContractAttrObjectType = types.ObjectType{
+		AttrTypes: map[string]attr.Type{
+			"name":   types.StringType,
+			"masked": types.BoolType,
+		},
+	}
+	extendedAttributesDefault, _ = types.SetValue(attributeContractAttrObjectType, nil)
+)
