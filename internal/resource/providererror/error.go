cee5b10bd10f4fe19faa9a12b19ced84cee310e7
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2024-09-17 13:29:38 -0500

    Server settings fixes (#390)
    * Validate url, mark deprecated attrs, and reset on delete for server_settings
    
    * Generate docs

--- internal/resource/providererror/error.go
+++ internal/resource/providererror/error.go
@@ -8,18 +8,19 @@ import (
 
 const (
 	// Common provider error summaries
-	InvalidProviderConfiguration   = "Invalid provider configuration"
-	InvalidAttributeConfiguration  = "Invalid attribute configuration"
-	InvalidResourceConfiguration   = "Invalid resource configuration"
-	InvalidProductVersionAttribute = "Invalid product_version for attribute"
-	InvalidProductVersionResource  = "Invalid product_version for resource"
-	InternalProviderError          = "Internal provider error"
-	PingFederateValidationError    = "PingFederate validation error"
-	PingFederateAPIError           = "PingFederate API error"
-	ConfigurationWarning           = "Plugin configuration warning"
+	InvalidProviderConfiguration    = "Invalid provider configuration"
+	InvalidAttributeConfiguration   = "Invalid attribute configuration"
+	InvalidResourceConfiguration    = "Invalid resource configuration"
+	InvalidProductVersionAttribute  = "Invalid product_version for attribute"
+	InvalidProductVersionResource   = "Invalid product_version for resource"
+	InternalProviderError           = "Internal provider error"
+	PingFederateValidationError     = "PingFederate validation error"
+	PingFederateAPIError            = "PingFederate API error"
+	ConfigurationWarning            = "Plugin configuration warning"
+	ConfigurationCannotBeResetError = "Configuration cannot be returned to original state"
 )
 
 func WarnConfigurationCannotBeReset(resourceName string, diags *diag.Diagnostics) {
-	diags.AddWarning("Configuration cannot be returned to original state",
+	diags.AddWarning(ConfigurationCannotBeResetError,
 		fmt.Sprintf("The %s resource has been destroyed but cannot be returned to its original state. The resource has been removed from Terraform state but the configuration remains applied to the environment", resourceName))
 }
