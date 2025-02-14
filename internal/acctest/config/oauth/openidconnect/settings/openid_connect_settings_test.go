7321d23cbe7b286d18d36a410a55776e5081aa56
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-02-14 17:09:23 -0600

    Simplify OIDC settings test to avoid dependency issues

--- internal/acctest/config/oauth/openidconnect/settings/openid_connect_settings_test.go
+++ internal/acctest/config/oauth/openidconnect/settings/openid_connect_settings_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/provider"
 )
@@ -25,48 +24,24 @@ func TestAccOpenIdConnectSettings(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccOpenIdConnectSettings(resourceName),
-				Check:  testAccCheckExpectedOpenIdConnectSettingsAttributes(),
 			},
 			{
 				// Test importing the resource
-				Config:                               testAccOpenIdConnectSettings(resourceName),
-				ResourceName:                         "pingfederate_openid_connect_settings." + resourceName,
-				ImportState:                          true,
-				ImportStateVerify:                    true,
-				ImportStateVerifyIdentifierAttribute: "default_policy_ref.id",
+				Config:       testAccOpenIdConnectSettings(resourceName),
+				ResourceName: "pingfederate_openid_connect_settings." + resourceName,
+				ImportState:  true,
 			},
 		},
 	})
 }
 
-func testAccOpenIdConnectSettings(resourceName string) string { //TODO fix this
+func testAccOpenIdConnectSettings(resourceName string) string {
 	// The dependent OIDC policy is not created in this test because prior to PF 12.1 it isn't possible to delete
-	// the final OIDC policy from the server config, because it is always in use.
+	// the final OIDC policy from the server config, because it is always in use. This would also interfere
+	// with other tests that make changes to OIDC policies or access token managers.
+	//TODO update this test once 12.1 is the oldest supported version, and create the OIDC policy in this test.
 	return fmt.Sprintf(`
 resource "pingfederate_openid_connect_settings" "%s" {
-  default_policy_ref = {
-    id = "oidcSettingsTestPolicy"
-  }
 }`, resourceName,
 	)
-}
-
-func testAccCheckExpectedOpenIdConnectSettingsAttributes() resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		resourceType := "OpenIdConnectSettings"
-		testClient := acctest.TestClient()
-		ctx := acctest.TestBasicAuthContext()
-		response, _, err := testClient.OauthOpenIdConnectAPI.GetOIDCSettings(ctx).Execute()
-		if err != nil {
-			return err
-		}
-
-		// Verify that attributes have expected values
-		err = acctest.TestAttributesMatchString(resourceType, nil, "id", "oidcSettingsTestPolicy", response.DefaultPolicyRef.Id)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
 }
