49623467c43cce08664c29e2d8c801b38486ff0f
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-04-16 15:06:41 -0500

    administrative_account test

--- internal/acctest/config/administrativeaccount/administrative_account_test.go
+++ internal/acctest/config/administrativeaccount/administrative_account_test.go
@@ -4,228 +4,153 @@ package administrativeaccount_test
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
-	client "github.com/pingidentity/pingfederate-go-client/v1220/configurationapi"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest"
-	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest/common/pointers"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/provider"
 )
 
-const username = "username"
+const administrativeAccountUsername = "administrativeAccountUsername"
 
-var password = "2FederateM0re!"
-
-type administrativeAccountResourceModel struct {
-	administrativeAccount *client.AdministrativeAccount
-}
-
-func initialAdministrativeAccount() *client.AdministrativeAccount {
-	initialAdministrativeAccount := client.NewAdministrativeAccountWithDefaults()
-	initialAdministrativeAccount.Username = username
-	initialAdministrativeAccount.Password = &password
-	initialAdministrativeAccount.Active = pointers.Bool(true)
-	initialAdministrativeAccount.Roles = []string{"USER_ADMINISTRATOR"}
-	return initialAdministrativeAccount
-}
-
-func updateAdministrativeAccount() *client.AdministrativeAccount {
-	updateAdministrativeAccount := client.NewAdministrativeAccountWithDefaults()
-	updateAdministrativeAccount.Username = username
-	updateAdministrativeAccount.Active = pointers.Bool(false)
-	updateAdministrativeAccount.Description = pointers.String("updated description")
-	updateAdministrativeAccount.Department = pointers.String("updated department")
-	updateAdministrativeAccount.EmailAddress = pointers.String("test@example.com")
-	updateAdministrativeAccount.PhoneNumber = pointers.String("555-555-5555")
-	updateAdministrativeAccount.Roles = []string{}
-	updateAdministrativeAccount.Auditor = pointers.Bool(true)
-	updateAdministrativeAccount.Department = pointers.String("department")
-	return updateAdministrativeAccount
-}
-
-func hcl(aa *client.AdministrativeAccount) string {
-	var builder strings.Builder
-	if aa == nil {
-		return ""
-	}
-	if aa != nil {
-		tf := `
-		%[1]s
-		%[2]s
-		%[3]s
-		%[4]s
-		%[5]s
-		%[6]s
-		%[7]s
-		%[8]s
-		%[9]s
-		`
-		auditor := ""
-		if aa.Auditor != nil {
-			auditor = strconv.FormatBool(*aa.Auditor)
-		}
-		builder.WriteString(
-			fmt.Sprintf(tf,
-				acctest.TfKeyValuePairToString("active", strconv.FormatBool(aa.GetActive()), true),
-				acctest.TfKeyValuePairToString("description", aa.GetDescription(), true),
-				acctest.TfKeyValuePairToString("department", aa.GetDepartment(), true),
-				acctest.TfKeyValuePairToString("email_address", aa.GetEmailAddress(), true),
-				acctest.TfKeyValuePairToString("roles", acctest.StringSliceToTerraformString(aa.Roles), false),
-				acctest.TfKeyValuePairToString("password", password, true),
-				acctest.TfKeyValuePairToString("phone_number", aa.GetPhoneNumber(), true),
-				acctest.TfKeyValuePairToString("username", aa.Username, true),
-				acctest.TfKeyValuePairToString("auditor", auditor, false),
-			),
-		)
-	}
-	return builder.String()
-}
-
-func testAccAdministrativeAccount(resourceName string, resourceModel administrativeAccountResourceModel) string {
-	return fmt.Sprintf(`
-resource "pingfederate_administrative_account" "%[1]s" {
-	%[2]s
-}
-
-data "pingfederate_administrative_account" "%[1]s" {
-  username = pingfederate_administrative_account.%[1]s.username
-}`,
-		resourceName,
-		hcl(resourceModel.administrativeAccount),
-	)
-}
-
-func testAccCheckExpectedAdministrativeAccountAttributes(config administrativeAccountResourceModel) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		testClient := acctest.TestClient()
-		ctx := acctest.TestBasicAuthContext()
-		response, _, err := testClient.AdministrativeAccountsAPI.GetAccount(ctx, config.administrativeAccount.Username).Execute()
-		if err != nil {
-			return err
-		}
-		resourceType := "AdministrativeAccount"
-		// Verify that attributes have expected values
-		err = acctest.TestAttributesMatchBool(resourceType, &config.administrativeAccount.Username, "active",
-			*config.administrativeAccount.Active, response.GetActive())
-		if err != nil {
-			return err
-		}
-
-		if config.administrativeAccount.Description != nil {
-			err = acctest.TestAttributesMatchString(resourceType, &config.administrativeAccount.Username, "description",
-				*config.administrativeAccount.Description, response.GetDescription())
-			if err != nil {
-				return err
-			}
-		}
-
-		if config.administrativeAccount.Department != nil {
-			err = acctest.TestAttributesMatchString(resourceType, &config.administrativeAccount.Username, "department",
-				*config.administrativeAccount.Department, response.GetDepartment())
-			if err != nil {
-				return err
-			}
-		}
-
-		if config.administrativeAccount.EmailAddress != nil {
-			err = acctest.TestAttributesMatchString(resourceType, &config.administrativeAccount.Username, "email_address",
-				*config.administrativeAccount.EmailAddress, response.GetEmailAddress())
-			if err != nil {
-				return err
-			}
-		}
-
-		if config.administrativeAccount.PhoneNumber != nil {
-			err = acctest.TestAttributesMatchString(resourceType, &config.administrativeAccount.Username, "phone_number",
-				*config.administrativeAccount.PhoneNumber, response.GetPhoneNumber())
-			if err != nil {
-				return err
-			}
-		}
-
-		err = acctest.TestAttributesMatchStringSlice(resourceType, &config.administrativeAccount.Username, "roles",
-			config.administrativeAccount.Roles, response.Roles)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-}
-
-// Test that any objects created by the test are destroyed
-func testAccCheckAdministrativeAccountDestroy(s *terraform.State) error {
-	testClient := acctest.TestClient()
-	ctx := acctest.TestBasicAuthContext()
-	_, err := testClient.AdministrativeAccountsAPI.DeleteAccount(ctx, username).Execute()
-	if err == nil {
-		return acctest.ExpectedDestroyError("AdministrativeAccount", username)
-	}
-	return nil
-}
-
-func TestAccAdministrativeAccount(t *testing.T) {
-	resourceName := "myAdministrativeAccount"
-	initialResourceModel := administrativeAccountResourceModel{
-		administrativeAccount: initialAdministrativeAccount(),
-	}
-
-	updatedResourceModel := administrativeAccountResourceModel{
-		administrativeAccount: updateAdministrativeAccount(),
-	}
-
+func TestAccAdministrativeAccount_RemovalDrift(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"pingfederate": providerserver.NewProtocol6WithError(provider.NewTestProvider()),
 		},
-		CheckDestroy: testAccCheckAdministrativeAccountDestroy,
+		CheckDestroy: administrativeAccount_CheckDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAdministrativeAccount(resourceName, initialResourceModel),
-				Check:  testAccCheckExpectedAdministrativeAccountAttributes(initialResourceModel),
+				// Create the resource with a minimal model
+				Config: administrativeAccount_MinimalHCL(),
 			},
 			{
-				Config: testAccAdministrativeAccount(resourceName, updatedResourceModel),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckExpectedAdministrativeAccountAttributes(updatedResourceModel),
-					resource.TestCheckResourceAttr(fmt.Sprintf("pingfederate_administrative_account.%s", resourceName), "active", "false"),
-				),
-			},
-			{
-				// Test importing the resource
-				Config:                  testAccAdministrativeAccount(resourceName, updatedResourceModel),
-				ResourceName:            "pingfederate_administrative_account." + resourceName,
-				ImportStateId:           initialResourceModel.administrativeAccount.Username,
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"password", "encrypted_password"},
-			},
-			{
-				Config: testAccAdministrativeAccount(resourceName, initialResourceModel),
-				Check:  testAccCheckExpectedAdministrativeAccountAttributes(initialResourceModel),
-			},
-			{
+				// Delete the resource on the service, outside of terraform, verify that a non-empty plan is generated
 				PreConfig: func() {
-					testClient := acctest.TestClient()
-					ctx := acctest.TestBasicAuthContext()
-					_, err := testClient.AdministrativeAccountsAPI.DeleteAccount(ctx, updatedResourceModel.administrativeAccount.Username).Execute()
-					if err != nil {
-						t.Fatalf("Failed to delete config: %v", err)
-					}
+					administrativeAccount_Delete(t)
 				},
 				RefreshState:       true,
 				ExpectNonEmptyPlan: true,
 			},
+		},
+	})
+}
+
+func TestAccAdministrativeAccount_MinimalMaximal(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"pingfederate": providerserver.NewProtocol6WithError(provider.NewTestProvider()),
+		},
+		CheckDestroy: administrativeAccount_CheckDestroy,
+		Steps: []resource.TestStep{
 			{
-				Config: testAccAdministrativeAccount(resourceName, initialResourceModel),
-				Check:  testAccCheckExpectedAdministrativeAccountAttributes(initialResourceModel),
+				// Create the resource with a minimal model
+				Config: administrativeAccount_MinimalHCL(),
+				Check:  administrativeAccount_CheckComputedValuesMinimal(),
+			},
+			{
+				// Delete the minimal model
+				Config:  administrativeAccount_MinimalHCL(),
+				Destroy: true,
+			},
+			{
+				// Re-create with a complete model
+				Config: administrativeAccount_CompleteHCL(),
+				Check:  administrativeAccount_CheckComputedValuesComplete(),
+			},
+			{
+				// Back to minimal model
+				Config: administrativeAccount_MinimalHCL(),
+				Check:  administrativeAccount_CheckComputedValuesMinimal(),
+			},
+			{
+				// Back to complete model
+				Config: administrativeAccount_CompleteHCL(),
+				Check:  administrativeAccount_CheckComputedValuesComplete(),
+			},
+			{
+				// Test importing the resource
+				Config:                               administrativeAccount_CompleteHCL(),
+				ResourceName:                         "pingfederate_administrative_account.example",
+				ImportStateId:                        administrativeAccountUsername,
+				ImportStateVerifyIdentifierAttribute: "username",
+				ImportState:                          true,
+				ImportStateVerify:                    true,
+				// Password can't be imported, and encrypted_password will change each time it is reaad
+				ImportStateVerifyIgnore: []string{"password", "encrypted_password"},
 			},
 		},
 	})
+}
+
+// Minimal HCL with only required values set
+func administrativeAccount_MinimalHCL() string {
+	return fmt.Sprintf(`
+resource "pingfederate_administrative_account" "example" {
+  username = "%s"
+  roles = ["USER_ADMINISTRATOR"]
+  password = "2FederateM0re!"
+}
+`, administrativeAccountUsername)
+}
+
+// Maximal HCL with all values set where possible
+func administrativeAccount_CompleteHCL() string {
+	return fmt.Sprintf(`
+resource "pingfederate_administrative_account" "example" {
+  username = "%s"
+  active = false
+  auditor = true
+  department = "mydepartment"
+  description = "mydescription"
+  email_address = "aggie@draynorvillage.example.com"
+  password = "2FederateM0re!"
+  phone_number = "555-555-5555"
+  roles = []
+}
+`, administrativeAccountUsername)
+}
+
+// Validate any computed values when applying minimal HCL
+func administrativeAccount_CheckComputedValuesMinimal() resource.TestCheckFunc {
+	return resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttr("pingfederate_administrative_account.example", "active", "false"),
+		resource.TestCheckResourceAttr("pingfederate_administrative_account.example", "auditor", "false"),
+		resource.TestCheckNoResourceAttr("pingfederate_administrative_account.example", "department"),
+		resource.TestCheckNoResourceAttr("pingfederate_administrative_account.example", "description"),
+		resource.TestCheckNoResourceAttr("pingfederate_administrative_account.example", "email_address"),
+		resource.TestCheckResourceAttrSet("pingfederate_administrative_account.example", "encrypted_password"),
+		resource.TestCheckNoResourceAttr("pingfederate_administrative_account.example", "phone_number"),
+	)
+}
+
+// Validate any computed values when applying complete HCL
+func administrativeAccount_CheckComputedValuesComplete() resource.TestCheckFunc {
+	return resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttrSet("pingfederate_administrative_account.example", "encrypted_password"),
+	)
+}
+
+// Delete the resource
+func administrativeAccount_Delete(t *testing.T) {
+	testClient := acctest.TestClient()
+	_, err := testClient.AdministrativeAccountsAPI.DeleteAccount(acctest.TestBasicAuthContext(), administrativeAccountUsername).Execute()
+	if err != nil {
+		t.Fatalf("Failed to delete config: %v", err)
+	}
+}
+
+// Test that any objects created by the test are destroyed
+func administrativeAccount_CheckDestroy(s *terraform.State) error {
+	testClient := acctest.TestClient()
+	_, err := testClient.AdministrativeAccountsAPI.DeleteAccount(acctest.TestBasicAuthContext(), administrativeAccountUsername).Execute()
+	if err == nil {
+		return fmt.Errorf("administrative_account still exists after tests. Expected it to be destroyed")
+	}
+	return nil
 }
