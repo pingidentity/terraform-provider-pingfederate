a25a5225e50191a482ec46fa34d9376b3d6a32fd
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-04-16 16:00:33 -0500

    authentication_selector test

--- internal/acctest/config/authenticationselector/authentication_selector_test.go
+++ internal/acctest/config/authenticationselector/authentication_selector_test.go
@@ -10,231 +10,185 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
-	client "github.com/pingidentity/pingfederate-go-client/v1220/configurationapi"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest"
-	"github.com/pingidentity/terraform-provider-pingfederate/internal/acctest/common/pointers"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/provider"
 )
 
-const authenticationSelectorsId = "selectorTest"
+const authenticationSelectorSelectorId = "authenticationSelectorSelectorId"
 
-// Attributes to test with. Add optional properties to test here if desired.
-type authenticationSelectorsResourceModel struct {
-	attributeContract                *client.AuthenticationSelectorAttributeContract
-	pluginDescriptorRef              client.ResourceLink
-	addOrUpdateAuthNContextAttribute string
-	enableNoMatchResultValue         string
-	enableNotInRequestResultValue    string
-	overrideAuthnContextForFlow      string
-}
-
-func TestAccAuthenticationSelector(t *testing.T) {
-	resourceName := "myAuthenticationSelector"
-
-	initialResourceModel := authenticationSelectorsResourceModel{
-		pluginDescriptorRef: client.ResourceLink{
-			Id: "com.pingidentity.pf.selectors.saml.SamlAuthnContextAdapterSelector",
-		},
-		addOrUpdateAuthNContextAttribute: "true",
-		enableNoMatchResultValue:         "false",
-		enableNotInRequestResultValue:    "false",
-		overrideAuthnContextForFlow:      "true",
-		attributeContract: &client.AuthenticationSelectorAttributeContract{
-			ExtendedAttributes: []client.AuthenticationSelectorAttribute{
-				{
-					Name: "result_value",
-				},
-			},
-		},
-	}
-
-	updatedResourceModel := authenticationSelectorsResourceModel{
-		pluginDescriptorRef: client.ResourceLink{
-			Id: "com.pingidentity.pf.selectors.saml.SamlAuthnContextAdapterSelector",
-		},
-		addOrUpdateAuthNContextAttribute: "false",
-		enableNoMatchResultValue:         "true",
-		enableNotInRequestResultValue:    "true",
-		overrideAuthnContextForFlow:      "false",
-		attributeContract: &client.AuthenticationSelectorAttributeContract{
-			ExtendedAttributes: []client.AuthenticationSelectorAttribute{
-				{
-					Name: "result_value2",
-				},
-			},
-		},
-	}
-
+func TestAccAuthenticationSelector_RemovalDrift(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"pingfederate": providerserver.NewProtocol6WithError(provider.NewTestProvider()),
 		},
-		CheckDestroy: testAccCheckAuthenticationSelectorDestroy,
+		CheckDestroy: authenticationSelector_CheckDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccAuthenticationSelector(resourceName, initialResourceModel),
-				Check:  testAccCheckExpectedAuthenticationSelectorAttributes(initialResourceModel),
+				// Create the resource with a minimal model
+				Config: authenticationSelector_MinimalHCL(),
 			},
 			{
-				// Test updating some fields
-				Config: testAccAuthenticationSelector(resourceName, updatedResourceModel),
-				Check:  testAccCheckExpectedAuthenticationSelectorAttributes(updatedResourceModel),
-			},
-			{
-				// Test importing the resource
-				Config:            testAccAuthenticationSelector(resourceName, updatedResourceModel),
-				ResourceName:      "pingfederate_authentication_selector." + resourceName,
-				ImportStateId:     authenticationSelectorsId,
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testAccAuthenticationSelector(resourceName, initialResourceModel),
-				Check:  testAccCheckExpectedAuthenticationSelectorAttributes(initialResourceModel),
-			},
-			{
+				// Delete the resource on the service, outside of terraform, verify that a non-empty plan is generated
 				PreConfig: func() {
-					testClient := acctest.TestClient()
-					ctx := acctest.TestBasicAuthContext()
-					_, err := testClient.AuthenticationSelectorsAPI.DeleteAuthenticationSelector(ctx, authenticationSelectorsId).Execute()
-					if err != nil {
-						t.Fatalf("Failed to delete config: %v", err)
-					}
+					authenticationSelector_Delete(t)
 				},
 				RefreshState:       true,
 				ExpectNonEmptyPlan: true,
-			},
-			{
-				Config: testAccAuthenticationSelector(resourceName, initialResourceModel),
-				Check:  testAccCheckExpectedAuthenticationSelectorAttributes(initialResourceModel),
 			},
 		},
 	})
 }
 
-func overrideAuthnContextForFlow(overrideAuthnContextForFlowVal string) string {
-	if acctest.VersionAtLeast("11.3.0") {
-		return fmt.Sprintf(`
-		{
-			name  = "Override AuthN Context for Flow"
-			value = "%s"
-		},`, overrideAuthnContextForFlowVal)
-	}
-	return ""
+func TestAccAuthenticationSelector_MinimalMaximal(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { acctest.ConfigurationPreCheck(t) },
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"pingfederate": providerserver.NewProtocol6WithError(provider.NewTestProvider()),
+		},
+		CheckDestroy: authenticationSelector_CheckDestroy,
+		Steps: []resource.TestStep{
+			{
+				// Create the resource with a minimal model
+				Config: authenticationSelector_MinimalHCL(),
+				Check:  authenticationSelector_CheckComputedValues(),
+			},
+			{
+				// Delete the minimal model
+				Config:  authenticationSelector_MinimalHCL(),
+				Destroy: true,
+			},
+			{
+				// Re-create with a complete model
+				Config: authenticationSelector_CompleteHCL(),
+				Check:  authenticationSelector_CheckComputedValues(),
+			},
+			{
+				// Back to minimal model
+				Config: authenticationSelector_MinimalHCL(),
+				Check:  authenticationSelector_CheckComputedValues(),
+			},
+			{
+				// Back to complete model
+				Config: authenticationSelector_CompleteHCL(),
+				Check:  authenticationSelector_CheckComputedValues(),
+			},
+			{
+				// Test importing the resource
+				Config:                               authenticationSelector_CompleteHCL(),
+				ResourceName:                         "pingfederate_authentication_selector.example",
+				ImportStateId:                        authenticationSelectorSelectorId,
+				ImportStateVerifyIdentifierAttribute: "selector_id",
+				ImportState:                          true,
+				ImportStateVerify:                    true,
+				ImportStateVerifyIgnore: []string{
+					"configuration.tables",
+					"configuration.fields",
+				},
+			},
+		},
+	})
 }
 
-func testAccAuthenticationSelector(resourceName string, resourceModel authenticationSelectorsResourceModel) string {
+// Minimal HCL with only required values set
+func authenticationSelector_MinimalHCL() string {
 	return fmt.Sprintf(`
-resource "pingfederate_authentication_selector" "%[1]s" {
-  selector_id = "%[2]s"
-  name        = "%[2]s"
+resource "pingfederate_authentication_selector" "example" {
+  selector_id = "%s"
+  attribute_contract = {
+    extended_attributes = [
+      {
+        name = "extendedattr"
+      }
+    ]
+  }
+  configuration = {
+  }
+  name = "myInitialSelector"
   plugin_descriptor_ref = {
-    id = "%[3]s"
+    id = "com.pingidentity.pf.selectors.saml.SamlAuthnContextAdapterSelector"
+  }
+}
+`, authenticationSelectorSelectorId)
+}
+
+// Maximal HCL with all values set where possible
+func authenticationSelector_CompleteHCL() string {
+	versionedConfigurationFields := ""
+	if acctest.VersionAtLeast("11.3.0") {
+		versionedConfigurationFields = `
+		{
+			name  = "Override AuthN Context for Flow"
+			value = "true"
+		},
+		`
+	}
+	return fmt.Sprintf(`
+resource "pingfederate_authentication_selector" "example" {
+  selector_id = "%s"
+  attribute_contract = {
+    extended_attributes = [
+	  {
+	    name = "another"
+	  },
+      {
+        name = "extendedattr"
+      }
+    ]
   }
   configuration = {
     tables = []
     fields = [
-			%[4]s
+		%s
       {
         name  = "Add or Update AuthN Context Attribute"
-        value = "%[5]s"
+        value = "true"
       },
       {
         name  = "Enable 'No Match' Result Value"
-        value = "%[6]s"
+        value = "true"
       },
       {
         name  = "Enable 'Not in Request' Result Value"
-        value = "%[7]s"
+        value = "true"
       }
     ]
   }
-  attribute_contract = {
-    extended_attributes = [
-      {
-        name = "%[8]s"
-      }
-    ]
+  name = "myUpdatedSelector"
+  plugin_descriptor_ref = {
+    id = "com.pingidentity.pf.selectors.saml.SamlAuthnContextAdapterSelector"
   }
-}`, resourceName,
-		authenticationSelectorsId,
-		resourceModel.pluginDescriptorRef.Id,
-		overrideAuthnContextForFlow(resourceModel.overrideAuthnContextForFlow),
-		resourceModel.addOrUpdateAuthNContextAttribute,
-		resourceModel.enableNoMatchResultValue,
-		resourceModel.enableNotInRequestResultValue,
-		resourceModel.attributeContract.ExtendedAttributes[0].Name,
+}
+`, authenticationSelectorSelectorId, versionedConfigurationFields)
+}
+
+// Validate any computed values when applying HCL
+func authenticationSelector_CheckComputedValues() resource.TestCheckFunc {
+	expectedFieldCount := "3"
+	if acctest.VersionAtLeast("11.3.0") {
+		expectedFieldCount = "4"
+	}
+	return resource.ComposeTestCheckFunc(
+		resource.TestCheckResourceAttr("pingfederate_authentication_selector.example", "configuration.tables_all.#", "0"),
+		resource.TestCheckResourceAttr("pingfederate_authentication_selector.example", "configuration.fields_all.#", expectedFieldCount),
 	)
 }
 
-// Test that the expected attributes are set on the PingFederate server
-func testAccCheckExpectedAuthenticationSelectorAttributes(config authenticationSelectorsResourceModel) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		resourceType := "AuthenticationSelector"
-		testClient := acctest.TestClient()
-		ctx := acctest.TestBasicAuthContext()
-		response, _, err := testClient.AuthenticationSelectorsAPI.GetAuthenticationSelector(ctx, authenticationSelectorsId).Execute()
-
-		if err != nil {
-			return err
-		}
-
-		// Verify that attributes have expected values
-		err = acctest.TestAttributesMatchString(resourceType, pointers.String(authenticationSelectorsId), "selector_id", authenticationSelectorsId, response.Id)
-		if err != nil {
-			return err
-		}
-
-		err = acctest.TestAttributesMatchString(resourceType, pointers.String(authenticationSelectorsId), "name", authenticationSelectorsId, response.Name)
-		if err != nil {
-			return err
-		}
-
-		configFields := response.Configuration.Fields
-		for _, field := range configFields {
-
-			if field.Name == "Add or Update AuthN Context Attribute" {
-				err = acctest.TestAttributesMatchString(resourceType, pointers.String(authenticationSelectorsId), "value", config.addOrUpdateAuthNContextAttribute, *field.Value)
-				if err != nil {
-					return err
-				}
-			}
-
-			if field.Name == "Enable No Match Result Value" {
-				err = acctest.TestAttributesMatchString(resourceType, pointers.String(authenticationSelectorsId), "value", config.enableNoMatchResultValue, *field.Value)
-				if err != nil {
-					return err
-				}
-			}
-
-			if field.Name == "Enable Not In Request Result Value" {
-				err = acctest.TestAttributesMatchString(resourceType, pointers.String(authenticationSelectorsId), "value", config.enableNotInRequestResultValue, *field.Value)
-				if err != nil {
-					return err
-				}
-			}
-
-		}
-
-		if config.attributeContract != nil {
-			err = acctest.TestAttributesMatchString(resourceType, pointers.String(authenticationSelectorsId), "name", config.attributeContract.ExtendedAttributes[0].Name, response.AttributeContract.ExtendedAttributes[0].Name)
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
+// Delete the resource
+func authenticationSelector_Delete(t *testing.T) {
+	testClient := acctest.TestClient()
+	_, err := testClient.AuthenticationSelectorsAPI.DeleteAuthenticationSelector(acctest.TestBasicAuthContext(), authenticationSelectorSelectorId).Execute()
+	if err != nil {
+		t.Fatalf("Failed to delete config: %v", err)
 	}
 }
 
 // Test that any objects created by the test are destroyed
-func testAccCheckAuthenticationSelectorDestroy(s *terraform.State) error {
+func authenticationSelector_CheckDestroy(s *terraform.State) error {
 	testClient := acctest.TestClient()
-	ctx := acctest.TestBasicAuthContext()
-	_, err := testClient.AuthenticationSelectorsAPI.DeleteAuthenticationSelector(ctx, authenticationSelectorsId).Execute()
+	_, err := testClient.AuthenticationSelectorsAPI.DeleteAuthenticationSelector(acctest.TestBasicAuthContext(), authenticationSelectorSelectorId).Execute()
 	if err == nil {
-		return acctest.ExpectedDestroyError("AuthenticationSelector", authenticationSelectorsId)
+		return fmt.Errorf("authentication_selector still exists after tests. Expected it to be destroyed")
 	}
 	return nil
 }
