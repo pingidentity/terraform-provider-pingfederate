544bf382850fc5294515b3da4d803a1e44d868bc
Henry Recker <henryrecker@pingidentity.com>
2026-01-20 14:29:33 -0600

    Regenerate cluster_settings resource and update ModifyPlan

--- internal/resource/config/cluster/settings/cluster_settings_resource.go
+++ internal/resource/config/cluster/settings/cluster_settings_resource.go
@@ -6,20 +6,39 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/providererror"
+	internaltypes "github.com/pingidentity/terraform-provider-pingfederate/internal/types"
 	"github.com/pingidentity/terraform-provider-pingfederate/internal/version"
 )
 
 func (r *clusterSettingsResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
-	// This endpoint was added in PingFederate 12.0
-	// Compare to version 12.0 of PF
+	// Compare to version 12.0.0 of PF
 	compare, err := version.Compare(r.providerConfig.ProductVersion, version.PingFederate1200)
 	if err != nil {
-		resp.Diagnostics.AddError(providererror.InternalProviderError, "Failed to compare PingFederate versions: "+err.Error())
+		resp.Diagnostics.AddError("Failed to compare PingFederate versions", err.Error())
 		return
 	}
-	pfVersionAtLeast120 := compare >= 0
-	if !pfVersionAtLeast120 {
+	pfVersionAtLeast1200 := compare >= 0
+	// Compare to version 13.0.0 of PF
+	compare, err = version.Compare(r.providerConfig.ProductVersion, version.PingFederate1300)
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to compare PingFederate versions", err.Error())
+		return
+	}
+	pfVersionAtLeast1300 := compare >= 0
+	// This endpoint was added in PingFederate 12.0
+	if !pfVersionAtLeast1200 {
 		version.AddUnsupportedResourceError("pingfederate_cluster_settings", r.providerConfig.ProductVersion, version.PingFederate1200, &resp.Diagnostics)
+	}
+	var plan *clusterSettingsResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if plan == nil {
+		return
+	}
+	// If any of these fields are set by the user and the PF version is not new enough, throw an error
+	if !pfVersionAtLeast1300 {
+		if internaltypes.IsDefined(plan.ReplicateLogSettingsOnSave) {
+			version.AddUnsupportedAttributeError("replicate_log_settings_on_save",
+				r.providerConfig.ProductVersion, version.PingFederate1300, &resp.Diagnostics)
+		}
 	}
 }
