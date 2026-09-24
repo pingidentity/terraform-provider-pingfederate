89fbc280ae06b21bf400d5386ae2968814b39db6
wesleymccollam <wesleymccollam@pingidentity.com>
2026-09-24 15:08:30 -0400

    CDI-1512: Add oauth_token_exchange_processor_policy_mapping resource

---
+++ internal/resource/config/oauth/tokenexchange/processor/policymappings/oauth_token_exchange_processor_policy_mapping_resource.go
@@ -0,0 +1,47 @@
+// Copyright © 2026 Ping Identity Corporation
+
+package oauthtokenexchangeprocessorpolicymappings
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/pingidentity/terraform-provider-pingfederate/internal/resource/providererror"
+	internaltypes "github.com/pingidentity/terraform-provider-pingfederate/internal/types"
+)
+
+func (r *oauthTokenExchangeProcessorPolicyMappingResource) ValidateConfig(ctx context.Context, req resource.ValidateConfigRequest, resp *resource.ValidateConfigResponse) {
+	var model *oauthTokenExchangeProcessorPolicyMappingResourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &model)...)
+
+	if model == nil || resp.Diagnostics.HasError() {
+		return
+	}
+
+	if internaltypes.IsDefined(model.AttributeContractFulfillment) {
+		userKeyFound := false
+		userNameFound := false
+		for key := range model.AttributeContractFulfillment.Elements() {
+			if key == "USER_KEY" {
+				userKeyFound = true
+			}
+			if key == "USER_NAME" {
+				userNameFound = true
+			}
+		}
+
+		if !userKeyFound {
+			resp.Diagnostics.AddAttributeError(
+				path.Root("attribute_contract_fulfillment"),
+				providererror.InvalidAttributeConfiguration,
+				"attribute_contract_fulfillment.USER_KEY is required")
+		}
+		if !userNameFound {
+			resp.Diagnostics.AddAttributeError(
+				path.Root("attribute_contract_fulfillment"),
+				providererror.InvalidAttributeConfiguration,
+				"attribute_contract_fulfillment.USER_NAME is required")
+		}
+	}
+}
